d4e1cf48bf96f27b066b89e62fa7e2787d9aeecd
Jake Meyer <jakewmeyer@gmail.com>
2017-06-19 14:20:28 -0500

    created basic sinatra API

---
+++ Gemfile
@@ -0,0 +1,6 @@
+source 'https://rubygems.org'
+gem 'sinatra'
+gem 'sinatra-subdomain'
+gem 'json'
+gem 'rest-client'
+gem 'sinatra-json'
---
+++ app.rb
@@ -0,0 +1,31 @@
+# SpaceX API for searching company info
+
+require 'sinatra'
+require 'sinatra/subdomain'
+require 'json'
+require 'rest-client'
+require 'sinatra/json'
+
+
+# Disables rack protection because of false positives
+# that were blocking connections to home page
+disable :protection
+
+
+# No longer necessary
+# Forces the use of HTTPS for the API
+#before do
+#  redirect request.url.sub('http', 'https') unless request.secure?
+#end
+
+
+# Uses subdomain api.exaample.com to route traffic
+subdomain :api do
+
+  # Home page for the API
+  get '/' do
+  content_type :json
+  home_response = { :Details => 'https://github.com/jakewmeyer/Helixbase-API', :Version => 'v1.000' }
+  JSON.pretty_generate(home_response)
+  end
+end
