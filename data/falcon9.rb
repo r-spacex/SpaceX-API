d644e2794a1d5d5e3276135792675f30c9f6de3f
Jake Meyer <jakewmeyer@gmail.com>
2017-06-19 17:34:08 -0500

    added data about falcon 9

--- data/falcon9.rb
+++ data/falcon9.rb
@@ -1,4 +1,59 @@
 $falcon9 = {
-  name: 'Falcon 9'
+  name: "Falcon 9",
+  height: {
+    meters: "70",
+    feet: "229.6"
+  },
+  diameter: {
+    meters: "3.7",
+    feet: "12"
+  },
+  mass: {
+    kg: "549,054",
+    lb: "1,207,920"
+  },
+  stages: "2",
+  payload_weight: {
+    leo: {
+      name: "Low Earth Orbit",
+      kg: "22,800",
+      lb: "50,265"
+    },
+    gto: {
+      name: "Geosynchronous Transfer Orbit",
+      kg: "8,300",
+      lb: "18,300"
+    },
+    mars: {
+      name: "Mars",
+      kg: "4,020",
+      lb: "8,860"
+    },
+  },
+  stage_1: {
+    engines: "9",
+    reusable: "true",
+    burn_time_sec: "162",
+    sea_level_thrust: {
+      kN: "7,607"
+      lbf: "1,710,000"
+    },
+    vacuum_thrust: {
+      kN: "8,227"
+      lbf: "1,849,500"
+    }
+  },
+  stage_2: {
+    engines: "1",
+    burn_time_sec: "397",
+    sea_level_thrust: {
+      kN: "934"
+      lbf: "210,000"
+    }
+  },
+  payload: {
+
+  }
+
 
 }
