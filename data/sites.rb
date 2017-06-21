0583291c0b4eb3dfa8ba448fd54b9503ca9343a6
Jake Meyer <jakewmeyer@gmail.com>
2017-06-21 04:04:18 -0500

    added initial site data

---
+++ data/sites.rb
@@ -0,0 +1,44 @@
+{
+  test_locations: {
+    name: 'Rocket Development Facility',
+    city: 'McGregor',
+    state: 'Texas',
+    size_acres: '4000',
+    year_built: '2003',
+    engines_tested: {
+      engine_1: 'Merlin',
+      engine_2: 'Draco'
+    },
+    yearly_tests: '400',
+    daily_tests: '2'
+  },
+  launch_locations: {
+    CCAFS: {
+      name: 'Cape Canaveral Air Force Station',
+      launchpad: 'Space Launch Complex 40',
+      state: 'Florida',
+      orbits_avaliable: '[LEO, GTO, ISS]',
+      vehicles: 'Falcon 9'
+    },
+    VAFB: {
+      name: 'Vandenberg Air Force Base',
+      launchpad: 'Space Launch Complex 4 East',
+      state: 'California',
+      orbits_avaliable: '[Polar, HIO]',
+      vehicles: 'Falcon 9'
+    },
+    KSC: {
+      name: 'Kennedy Space Center',
+      launchpad: 'Launch Complex 39A',
+      state: 'Florida',
+      orbits_avaliable: '[LEO, GTO, ISS]',
+      vehicles: 'Falcon 9'
+    },
+    STLS: {
+      name: 'South Texas Launch Site',
+      launchpad: 'custom pad',
+      state: 'Texas',
+      orbits_avaliable: '[LEO, GTO, ISS]'
+    }
+  }
+ }
