ae5760a630f72f8535960e82a95fa00acd9047ba
Jake Meyer <jakewmeyer@gmail.com>
2017-06-22 22:48:45 -0500

    numbers and bools aren't strings anymore

--- data/falcon_heavy.rb
+++ data/falcon_heavy.rb
@@ -1,102 +1,102 @@
 $falcon_heavy = {
   name: 'Falcon Heavy',
-  active: 'false',
-  stages: '2',
-  boosters: '2',
-  cost_per_launch: '90 million',
+  active: false,
+  stages: 2,
+  boosters: 2,
+  cost_per_launch: 90000000,
   first_flight: 'TBD',
   country: 'United States',
   company: 'SpaceX',
   height: {
-    meters: '70',
-    feet: '229.6'
+    meters: 70,
+    feet: 229.6
   },
   total_width: {
-    meters: '12.2',
-    feet: '39.9'
+    meters: 12.2,
+    feet: 39.9
   },
   mass: {
-    kg: '1420788',
-    lb: '3125735'
+    kg: 1420788,
+    lb: 3125735
   },
   payload_weights: {
     leo: {
       name: 'low earth orbit',
-      kg: '63800',
-      lb: '140660'
+      kg: 63800,
+      lb: 140660
     },
     gto: {
       name: 'geosynchronous transfer orbit',
-      kg: '26700',
-      lb: '58860'
+      kg: 26700,
+      lb: 58860
     },
     mars: {
       name: 'mars orbit',
-      kg: '16800',
-      lb: '37040'
+      kg: 16800,
+      lb: 37040
     },
     pluto: {
       name: 'pluto orbit',
-      kg: '3500',
-      lb: '7720'
+      kg: 3500,
+      lb: 7720
     }
   },
   first_stage: {
     reusable: 'true',
-    engines: '27',
-    cores: '3',
-    burn_time_sec: '162',
+    engines: 27,
+    cores: 3,
+    burn_time_sec: 162,
     thrust_sea_level: {
-      kN: '22819',
-      lbf: '5130000'
+      kN: 22819,
+      lbf: 5130000
     },
     thrust_vacuum: {
-      kN: '24681',
-      lbf: '5548500'
+      kN: 24681,
+      lbf: 5548500
     }
   },
   second_stage: {
-    engines: '1',
-    burn_time_sec: '397',
+    engines: 1,
+    burn_time_sec: 397,
     thrust: {
-      kN: '934',
-      lbf: '210000'
+      kN: 934,
+      lbf: 210000
     },
     payloads: {
       option_1: 'dragon',
       option_2: 'composite fairing',
       composite_fairing: {
         height: {
-          meters: '13.1',
-          feet: '43'
+          meters: 13.1,
+          feet: 43
         },
         diameter: {
-          meters: '5.2',
-          feet: '17.1'
+          meters: 5.2,
+          feet: 17.1
         }
       }
     }
   },
   engines: {
-    number: '27',
+    number: 27,
     type: 'merlin',
     version: '1D+',
     layout: 'octaweb',
-    engine_loss_max: '6',
+    engine_loss_max: 6,
     propellant_1: 'oxygen',
     propellant_2: 'kerosene',
     thrust_sea_level: {
-      kN: '845',
-      lbf: '190000'
+      kN: 845,
+      lbf: 190000
     },
     thrust_vacuum: {
-      kN: '914',
-      lbf: '205500'
+      kN: 914,
+      lbf: 205500
     },
-    thrust_to_weight: '180.1'
+    thrust_to_weight: 180.1
   },
   landing_legs: {
-    number: '12',
+    number: 12,
     material: 'carbon fiber'
   },
   description: 'With the ability to lift into orbit over 54 metric tons (119,000 lb)--a mass equivalent to a 737 jetliner loaded with passengers, crew, luggage and fuel--Falcon Heavy can lift more than twice the payload of the next closest operational vehicle, the Delta IV Heavy, at one-third the cost.'
