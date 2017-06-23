77fa8fdae495d89b5188df9152ae86195f710c87
Jake Meyer <jakewmeyer@gmail.com>
2017-06-23 15:40:02 -0500

    bump version to v1.0.3

--- data/home_info.rb
+++ data/home_info.rb
@@ -1,6 +1,6 @@
 $home_info = {
   description: 'JSON API for data about company info, vehicles, launch sites, and launch data.',
   project_link: 'https://github.com/jakewmeyer/SpaceX-API',
-  version: '1.0.1',
+  version: '1.0.3',
   author: 'Jake Meyer'
 }
