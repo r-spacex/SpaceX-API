527bd0e09ee72782e08df95069babb1aa2c4944b
Jake Meyer <jakewmeyer@gmail.com>
2017-06-22 22:52:06 -0500

    numbers and bools aren't strings anymore for dragon

--- data/dragon.rb
+++ data/dragon.rb
@@ -1,8 +1,8 @@
 $dragon = {
   name: 'Dragon 1',
-  active: 'true',
-  sidewall_angle_deg: '15',
-  orbit_duration_yr: '2',
+  active: true,
+  sidewall_angle_deg: 15,
+  orbit_duration_yr: 2,
   variations: {
     cargo: {
       details: 'Resupply the space station and return cargo to earth.'
@@ -15,60 +15,60 @@ $dragon = {
     }
   },
   heat_shield: {
-    material: 'PICA-X',
-    size_meters: '3.6',
-    temp_degrees: '3000',
-    dev_partner: 'NASA'
+    material: PICA-X,
+    size_meters: 3.6,
+    temp_degrees: 3000,
+    dev_partner: NASA
   },
   thrusters: {
     type: 'Draco',
-    amount: '18',
-    pods: '4',
+    amount: 18,
+    pods: 4,
     fuel_1: 'nitrogen tetroxide',
     fuel_2: 'monomethylhydrazine',
     thrust: {
-      N: '400',
-      lbf: '90'
+      N: 400,
+      lbf: 90
     }
   },
   launch_payload_mass: {
-    kg: '6000',
-    lb: '13228'
+    kg: 6000,
+    lb: 13228
   },
   launch_payload_vol: {
-    cubic_meters: '25',
-    cubic_feet: '883'
+    cubic_meters: 25,
+    cubic_feet: 883
   },
   return_payload_mass: {
-    kg: '3000',
-    lb: '6614'
+    kg: 3000,
+    lb: 6614
   },
   return_payload_vol: {
-    cubic_meters: '11',
-    cubic_feet: '388'
+    cubic_meters: 11,
+    cubic_feet: 388
   },
   pressurized_capsule: {
     payload_volume: {
-      cubic_meters: '11',
-      cubic_feet: '388'
+      cubic_meters: 11,
+      cubic_feet: 388
     }
   },
   trunk: {
     trunk_volume: {
-      cubic_meters: '14',
-      cubic_feet: '494'
+      cubic_meters: 14,
+      cubic_feet: 494
     },
     cargo: {
-      solar_array: '2',
-      unpressurized_cargo: 'true'
+      solar_array: 2,
+      unpressurized_cargo: true
     }
   },
   height_w_trunk: {
-    meters: '7.2',
-    feet: '23.6'
+    meters: 7.2,
+    feet: 23.6
   },
   diameter: {
-    meters: '3.7',
-    feet: '12'
+    meters: 3.7,
+    feet: 12
   }
  }
