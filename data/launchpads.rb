33c74dacc9c84aad1e0fe6f49b21d28f2f2e8ab8
Brendan Kirchner <brendankirchner@me.com>
2017-07-11 00:55:21 -0400

    Update launchpads.rb

--- data/launchpads.rb
+++ data/launchpads.rb
@@ -10,11 +10,7 @@ $launchpads = {
             region: 'Texas',
             latitude: 31.399045,
             longitude: -97.462644
-<<<<<<< HEAD
-        }
-=======
         },
->>>>>>> r-spacex/master
         engines_tested: [
             'Merlin',
             'Draco'
