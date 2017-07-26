39b43903a4301bfc2a7497d60570f01a09255222
Jake Meyer <jakewmeyer@gmail.com>
2017-07-26 06:44:45 -0500

    removed conditional from docker script

--- docker_build.sh
+++ docker_build.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env sh
-if [ "$TRAVIS_BRANCH" == "master" ]; then
     curl -H "Content-Type: application/json" --data '{"build": true}' -X POST https://registry.hub.docker.com/u/jakewmeyer/spacex-api/trigger/5e92e617-8b11-489f-a269-36ab24a4d7a5/;
-fi
+    
