0e8b85abed152ecf1e75a47cb8c1087b0eda3d98
Jake Meyer <jakewmeyer@gmail.com>
2017-07-26 06:40:46 -0500

    revised docker build script

--- docker_build.sh
+++ docker_build.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/env sh
-curl -H "Content-Type: application/json" --data '{"build": true}' -X POST https://registry.hub.docker.com/u/jakewmeyer/spacex-api/trigger/5e92e617-8b11-489f-a269-36ab24a4d7a5/;
+if [ "$TRAVIS_BRANCH" == "master" ]; then
+    curl -H "Content-Type: application/json" --data '{"build": true}' -X POST https://registry.hub.docker.com/u/jakewmeyer/spacex-api/trigger/5e92e617-8b11-489f-a269-36ab24a4d7a5/;
+fi
