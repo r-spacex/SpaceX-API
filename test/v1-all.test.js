f3141eb13e903dafb5ac9f3e144b500e104eacea
Jake Meyer <jakewmeyer@gmail.com>
2017-08-13 20:50:46 -0500

    added error tests for all endpoints

--- test/v1-all.test.js
+++ test/v1-all.test.js
@@ -74,6 +74,12 @@ test("It should return LC-39A info", () => {
   })
 })
 
+test("It should return no launchpads found info", () => {
+  return request(app).get("/v1/launchpads/ksc_lc_40a").then(response => {
+    expect(response.statusCode).toBe(200)
+  })
+})
+
 // Past Launches Tests
 test("It should return all past launches", () => {
   return request(app).get("/v1/launches").then(response => {
@@ -93,8 +99,20 @@ test("It should return all 2012 launches", () => {
   })
 })
 
+test("It should return no 2005 launches", () => {
+  return request(app).get("/v1/launches?year=2005").then(response => {
+    expect(response.statusCode).toBe(200)
+  })
+})
+
 test("It should return past launches in timeframe", () => {
   return request(app).get("/v1/launches?start=2011-01-20&final=2017-05-25").then(response => {
+    expect(response.statusCode).toBe(200)
+  })
+})
+
+test("It should return no past launches in timeframe", () => {
+  return request(app).get("/v1/launches?start=2005-01-20&final=2005-05-25").then(response => {
     expect(response.statusCode).toBe(200)
   })
 })
@@ -105,8 +123,20 @@ test("It should return all launches with core B1021", () => {
   })
 })
 
+test("It should return no launches with core A1021", () => {
+  return request(app).get("/v1/launches/cores/A1021").then(response => {
+    expect(response.statusCode).toBe(200)
+  })
+})
+
 test("It should return all launches with cap C106", () => {
   return request(app).get("/v1/launches/caps/C106").then(response => {
+    expect(response.statusCode).toBe(200)
+  })
+})
+
+test("It should return no launches with cap C403", () => {
+  return request(app).get("/v1/launches/caps/C403").then(response => {
     expect(response.statusCode).toBe(200)
   })
 })
@@ -124,8 +154,20 @@ test("It should return all upcoming launches in 2017", () => {
   })
 })
 
+test("It should return no upcoming launches in 2016", () => {
+  return request(app).get("/v1/launches/upcoming?year=2016").then(response => {
+    expect(response.statusCode).toBe(200)
+  })
+})
+
 test("It should return all launches in the timeframe", () => {
   return request(app).get("/v1/launches/upcoming?start=2011-01-20&final=2017-05-25").then(response => {
+    expect(response.statusCode).toBe(200)
+  })
+})
+
+test("It should return no launches in the timeframe", () => {
+  return request(app).get("/v1/launches/upcoming?start=2011-01-20&final=2016-05-25").then(response => {
     expect(response.statusCode).toBe(200)
   })
 })
@@ -143,6 +185,12 @@ test("It should return all info on C106", () => {
   })
 })
 
+test("It should return np info on C406", () => {
+  return request(app).get("/v1/parts/caps/C406").then(response => {
+    expect(response.statusCode).toBe(200)
+  })
+})
+
 test("It should return all core info", () => {
   return request(app).get("/v1/parts/cores").then(response => {
     expect(response.statusCode).toBe(200)
@@ -151,6 +199,12 @@ test("It should return all core info", () => {
 
 test("It should return core info on B1021", () => {
   return request(app).get("/v1/parts/cores/B1021").then(response => {
+    expect(response.statusCode).toBe(200)
+  })
+})
+
+test("It should return no core info on A1021", () => {
+  return request(app).get("/v1/parts/cores/A1021").then(response => {
     expect(response.statusCode).toBe(200)
   })
 })
