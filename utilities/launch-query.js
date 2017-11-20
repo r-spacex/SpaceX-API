7c7c6ebed61dfed892872aff933d22e994096ea3
Jake Meyer <jakewmeyer@gmail.com>
2017-11-20 16:54:15 -0600

    Added type conversions for query builder

--- utilities/launch-query.js
+++ utilities/launch-query.js
@@ -3,6 +3,12 @@
 exports.queryBuilder = (req) => {
   let query = {}
 
+  if (req.query.start && req.query.final) {
+    query.launch_date_utc = {"$gte": `${req.query.start}T00:00:00Z`, "$lte": `${req.query.final}T00:00:00Z`}
+  }
+  if (req.query.flight_number) {
+    query.flight_number = parseInt(req.query.flight_number)
+  }
   if (req.query.launch_year) {
     query.launch_year = req.query.launch_year
   }
@@ -11,9 +17,6 @@ exports.queryBuilder = (req) => {
   }
   if (req.query.launch_date_local) {
     query.launch_date_local = req.query.launch_date_local
-  }
-  if (req.query.start && req.query.final) {
-    query.launch_date_utc = {"$gte": `${req.query.start}T00:00:00Z`, "$lte": `${req.query.final}T00:00:00Z`}
   }
   if (req.query.site_id) {
     query["launch_site.site_id"] = req.query.site_id
@@ -37,28 +40,28 @@ exports.queryBuilder = (req) => {
     query.cap_serial = req.query.cap_serial
   }
   if (req.query.core_reuse) {
-    query["reuse.core"] = req.query.core_reuse
+    query["reuse.core"] = Boolean(req.query.core_reuse)
   }
   if (req.query.side_core1_reuse) {
-    query["reuse.side_core1"] = req.query.side_core1_reuse
+    query["reuse.side_core1"] = Boolean(req.query.side_core1_reuse)
   }
   if (req.query.side_core2_reuse) {
-    query["reuse.side_core2"] = req.query.side_core2_reuse
+    query["reuse.side_core2"] = Boolean(req.query.side_core2_reuse)
   }
   if (req.query.fairings_reuse) {
-    query["reuse.fairings"] = req.query.fairings_reuse
+    query["reuse.fairings"] = Boolean(req.query.fairings_reuse)
   }
   if (req.query.capsule_reuse) {
-    query["reuse.capsule"] = req.query.capsule_reuse
+    query["reuse.capsule"] = Boolean(req.query.capsule_reuse)
   }
   if (req.query.launch_success) {
-    query.launch_success = req.query.launch_success
+    query.launch_success = Boolean(req.query.launch_success)
   }
   if (req.query.reused) {
-    query.reused = req.query.reused
+    query.reused = Boolean(req.query.reused)
   }
   if (req.query.land_success) {
-    query.land_success = req.query.land_success
+    query.land_success = Boolean(req.query.land_success)
   }
   if (req.query.landing_type) {
     query.landing_type = req.query.landing_type
