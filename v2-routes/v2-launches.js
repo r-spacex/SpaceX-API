86b92d9556a7023ea11c6b4f4f085c76f8e55fb7
Jake Meyer <jakewmeyer@gmail.com>
2017-11-22 11:54:07 -0600

    switched v2 routes over to v2 collections in mongodb

--- v2-routes/v2-launches.js
+++ v2-routes/v2-launches.js
@@ -6,7 +6,7 @@ const launch = require("../utilities/launch-query")
 
 // Get most recent launch
 v2.get("/latest", (req, res, next) => {
-  global.db.collection("launch").find({},{"_id": 0 }).sort({"flight_number": -1}).limit(1)
+  global.db.collection("launch_v2").find({},{"_id": 0 }).sort({"flight_number": -1}).limit(1)
     .toArray((err, doc) => {
       if (err) {
         return next(err)
@@ -18,7 +18,7 @@ v2.get("/latest", (req, res, next) => {
 // All past launches filtered by any querystring
 v2.get("/", (req, res, next) => {
   let query = launch.launchQuery(req)
-  global.db.collection("launch").find(query,{"_id": 0 }).sort({"flight_number": 1})
+  global.db.collection("launch_v2").find(query,{"_id": 0 }).sort({"flight_number": 1})
     .toArray((err, doc) => {
       if (err) {
         return next(err)
--- v2-routes/v2-upcoming.js
+++ v2-routes/v2-upcoming.js
@@ -7,7 +7,7 @@ const launch = require("../utilities/launch-query")
 // Upcoming launches by date, year, or all
 v2.get("/", (req, res, next) => {
   let query = launch.launchQuery(req)
-  global.db.collection("upcoming").find(query,{"_id": 0 }).sort({"flight_number": 1})
+  global.db.collection("upcoming_v2").find(query,{"_id": 0 }).sort({"flight_number": 1})
     .toArray((err, doc) => {
       if (err) {
         return next(err)
