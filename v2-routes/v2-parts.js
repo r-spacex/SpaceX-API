a5c63d5e036d439702daa71208447081462f6814
Jake Meyer <jakewmeyer@gmail.com>
2017-11-23 16:33:33 -0600

    removed redundant cap and core endpoints

--- v2-routes/v2-parts.js
+++ v2-routes/v2-parts.js
@@ -4,7 +4,6 @@ const express = require("express")
 const v2 = express.Router()
 const cores = require("../builders/core-query")
 const caps = require("../builders/capsule-query")
-const error = {error: "No results found"}
 
 // Returns all capsule information
 v2.get("/caps", (req, res, next) => {
@@ -18,22 +17,6 @@ v2.get("/caps", (req, res, next) => {
     })
 })
 
-// Returns capsule info by serial #
-v2.get("/caps/:cap", (req, res, next) => {
-  const cap = req.params.cap
-  global.db.collection("capsule").find({"capsule_serial": `${cap}`},{"_id": 0}).sort({"capsule_serial": 1})
-    .toArray((err, doc) => {
-      if (err) {
-        return next(err)
-      }
-      if (doc.length == 0) {
-        res.status(404)
-        return res.json(error)
-      }
-      res.json(doc[0])
-    })
-})
-
 // Returns all core information
 v2.get("/cores", (req, res, next) => {
   let query = cores.coreQuery(req)
@@ -43,22 +26,6 @@ v2.get("/cores", (req, res, next) => {
         return next(err)
       }
       res.json(doc)
-    })
-})
-
-// Returns core info by serial #
-v2.get("/cores/:core", (req, res, next) => {
-  const core = req.params.core
-  global.db.collection("core").find({"core_serial": `${core}`},{"_id": 0}).sort({"core_serial": 1})
-    .toArray((err, doc) => {
-      if (err) {
-        return next(err)
-      }
-      if (doc.length == 0) {
-        res.status(404)
-        return res.json(error)
-      }
-      res.json(doc[0])
     })
 })
 
