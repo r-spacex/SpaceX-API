c536e622fa1b280d70c3e76e8fc3ff2971c0e6e3
Jake Meyer <jakewmeyer@gmail.com>
2017-11-23 02:03:57 -0600

    moved v2 tests to new file

--- test/v1-all.test.js
+++ test/v1-all.test.js
@@ -1,6 +1,6 @@
 
-const app = require("../app")
 const request = require("supertest")
+const app = require("../app")
 const customMatchers = require("./utilities/custom-asymmetric-matchers")
 
 beforeAll((done) => {
@@ -480,71 +480,6 @@ test("It should return all past RTLS launches", () => {
   return request(app).get("/v1/launches/rtls").then(response => {
     expect(response.statusCode).toBe(200)
     expect(response.text).toContain("RTLS")
-  })
-})
-
-//------------------------------------------------------------
-//                    Past Launches V2
-//------------------------------------------------------------
-
-test("It should return all past launches", () => {
-  return request(app).get("/v2/launches").then(response => {
-    expect(response.statusCode).toBe(200)
-    response.body.forEach(item => {
-      expect(item).toHaveProperty("flight_number", expect.anything())
-      expect(item).toHaveProperty("launch_year", expect.stringMatching(/^[0-9]{4}$/))
-      expect(item).toHaveProperty("launch_date_unix")
-      expect(item).toHaveProperty("launch_date_utc", expect.anything())
-      expect(item).toHaveProperty("launch_date_local", expect.anything())
-      expect(item).toHaveProperty("rocket.rocket_id")
-      expect(item).toHaveProperty("rocket.rocket_name")
-      expect(item).toHaveProperty("rocket.rocket_type")
-      expect(item.rocket.first_stage.cores.length).toBeGreaterThan(0)
-      item.rocket.first_stage.cores.forEach(core => {
-        expect(core).toHaveProperty("core_serial")
-        expect(core).toHaveProperty("reused")
-        expect(core).toHaveProperty("land_success")
-        expect(core).toHaveProperty("landing_type")
-        expect(core).toHaveProperty("landing_vehicle")
-      })
-      expect(item.rocket.second_stage.payloads.length).toBeGreaterThan(0)
-      if (item.hasOwnProperty("cap_serial")) {
-        item.rocket.second_stage.payloads.forEach(payload => {
-          expect(payload).toHaveProperty("payload_id")
-          expect(payload).toHaveProperty("reused")
-          expect(payload).toHaveProperty("cap_serial")
-          expect(payload.customers.length).toBeGreaterThan(0)
-          expect(payload).toHaveProperty("payload_mass_kg")
-          expect(payload).toHaveProperty("payload_mass_lbs")
-          expect(payload).toHaveProperty("orbit")
-          expect(payload).toHaveProperty("mass_returned_kg")
-          expect(payload).toHaveProperty("mass_returned_lbs")
-          expect(payload).toHaveProperty("flight_time_sec")
-          expect(payload).toHaveProperty("cargo_manifest")
-        })
-      } else {
-        item.rocket.second_stage.payloads.forEach(payload => {
-          expect(payload).toHaveProperty("payload_id")
-          expect(payload).toHaveProperty("reused")
-          expect(payload.customers.length).toBeGreaterThan(0)
-          expect(payload).toHaveProperty("payload_mass_kg")
-          expect(payload).toHaveProperty("payload_mass_lbs")
-          expect(payload).toHaveProperty("orbit")
-        })
-      }
-      expect(item).toHaveProperty("telemetry.flight_club")
-      expect(item).toHaveProperty("reuse.core")
-      expect(item).toHaveProperty("reuse.side_core1")
-      expect(item).toHaveProperty("reuse.side_core2")
-      expect(item).toHaveProperty("reuse.fairings")
-      expect(item).toHaveProperty("reuse.capsule")
-      expect(item).toHaveProperty("launch_site.site_id")
-      expect(item).toHaveProperty("launch_site.site_name")
-      expect(item).toHaveProperty("launch_site.site_name_long")
-      expect(item).toHaveProperty("launch_success")
-      expect(item).toHaveProperty("links")
-      expect(item).toHaveProperty("details")
-    })
   })
 })
 
