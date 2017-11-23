ee461a15ce1a3f89e1df7d1b8008213f2166921d
Jake Meyer <jakewmeyer@gmail.com>
2017-11-23 02:51:38 -0600

    added tests for v2 core and capsule endpoints

--- test/v2-all.test.js
+++ test/v2-all.test.js
@@ -375,7 +375,7 @@ test("It should return all past launches", () => {
 //                    Upcoming Launches V2
 //------------------------------------------------------------
 
-test("It should return all past launches", () => {
+test("It should return all upcoming launches", () => {
   return request(app.listen()).get("/v2/launches/upcoming").then(response => {
     expect(response.statusCode).toBe(200)
     response.body.forEach(item => {
@@ -431,6 +431,47 @@ test("It should return all past launches", () => {
       expect(item).toHaveProperty("launch_site.site_name_long")
       expect(item).toHaveProperty("launch_success")
       expect(item).toHaveProperty("links")
+      expect(item).toHaveProperty("details")
+    })
+  })
+})
+
+//------------------------------------------------------------
+//                     Capsule V2
+//------------------------------------------------------------
+
+test("It should return all v2 capsules", () => {
+  return request(app).get("/v2/parts/caps").then(response => {
+    expect(response.statusCode).toBe(200)
+    response.body.forEach(item => {
+      expect(item).toHaveProperty("capsule_serial")
+      expect(item).toHaveProperty("status")
+      expect(item).toHaveProperty("original_launch")
+      expect(item.missions.length).toBeGreaterThan(0)
+      expect(item).toHaveProperty("landings")
+      expect(item).toHaveProperty("type")
+      expect(item).toHaveProperty("details")
+    })
+  })
+})
+
+//------------------------------------------------------------
+//                     Core V2
+//------------------------------------------------------------
+
+test("It should return all v2 cores", () => {
+  return request(app).get("/v2/parts/cores").then(response => {
+    expect(response.statusCode).toBe(200)
+    response.body.forEach(item => {
+      expect(item).toHaveProperty("core_serial")
+      expect(item).toHaveProperty("status")
+      expect(item).toHaveProperty("original_launch")
+      expect(item.missions.length).toBeGreaterThan(0)
+      expect(item).toHaveProperty("rtls_attempt")
+      expect(item).toHaveProperty("rtls_landings")
+      expect(item).toHaveProperty("asds_attempt")
+      expect(item).toHaveProperty("asds_landings")
+      expect(item).toHaveProperty("water_landing")
       expect(item).toHaveProperty("details")
     })
   })
