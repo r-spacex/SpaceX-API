004111cef183153a68a81208a08be2df7f285e91
Jake Meyer <jakewmeyer@gmail.com>
2017-11-30 07:20:51 -0600

    changed rate limit to actual value

--- app.js
+++ app.js
@@ -19,9 +19,8 @@ const v2_parts  = require("./v2-routes/v2-parts")
 
 // API rate limit of 9000 reqs / hour
 const limiter = new RateLimit({
-  // windowMs: 2250 * 60 * 1000,
-  windowMs: 100 * 60 * 1000,
-  max: 100,
+  windowMs: 2250 * 60 * 1000,
+  max: 2250,
   delayMs: 0,
   message: "API rate limit reached"
 })
