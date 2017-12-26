77fd91f5401847bc2423676b99da9c3544386ab8
Jake Meyer <jakewmeyer@gmail.com>
2017-12-25 21:45:14 -0600

    made all querystrings lowercase by default

--- src/builders/launch-sort.js
+++ src/builders/launch-sort.js
@@ -2,12 +2,14 @@
  * Returns sorting query for past/upcoming launches
 */
 
+const lowerCase = require("lower-case")
+
 exports.launchSort = (req) => {
   const query = {}
 
-  if (req.query.order === "asc") {
+  if (lowerCase(req.query.order) === "asc") {
     query.flight_number = 1
-  } else if (req.query.order === "desc") {
+  } else if (lowerCase(req.query.order) === "desc") {
     query.flight_number = -1
   } else {
     query.flight_number = 1
