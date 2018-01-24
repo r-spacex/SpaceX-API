4e4ca276f873b3da3bc5363ed7132eab8b935dc3
Jake Meyer <jakewmeyer@gmail.com>
2018-01-24 09:27:38 -0600

    update parts to async

--- src/v2-routes/v2-parts.js
+++ src/v2-routes/v2-parts.js
@@ -2,31 +2,30 @@
 
 const express = require("express")
 const v2 = express.Router()
+const asyncHandle = require("express-async-handler")
 const cores = require("../builders/core-query")
 const caps = require("../builders/capsule-query")
 
 // Returns all capsule information
-v2.get("/caps", (req, res, next) => {
+v2.get("/caps", asyncHandle(async (req, res) => {
   const query = caps.capsuleQuery(req)
-  global.db.collection("capsule").find(query,{"_id": 0}).sort({"capsule_serial": 1})
-    .toArray((err, doc) => {
-      if (err) {
-        return next(err)
-      }
-      res.json(doc)
-    })
-})
+  const data = await global.db
+    .collection("capsule")
+    .find(query,{"_id": 0})
+    .sort({"capsule_serial": 1})
+    .toArray()
+  res.json(data)
+}))
 
 // Returns all core information
-v2.get("/cores", (req, res, next) => {
+v2.get("/cores", asyncHandle(async (req, res) => {
   const query = cores.coreQuery(req)
-  global.db.collection("core").find(query,{"_id": 0}).sort({"core_serial": 1})
-    .toArray((err, doc) => {
-      if (err) {
-        return next(err)
-      }
-      res.json(doc)
-    })
-})
+  const data = await global.db
+    .collection("core")
+    .find(query,{"_id": 0})
+    .sort({"core_serial": 1})
+    .toArray()
+  res.json(data)
+}))
 
 module.exports = v2
