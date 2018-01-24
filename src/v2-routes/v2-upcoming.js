523b4eb4d1dffd3b1ec5735e3a6195f05fa5eec2
Jake Meyer <jakewmeyer@gmail.com>
2018-01-24 09:46:42 -0600

    update upcoming to async

--- src/v2-routes/v2-upcoming.js
+++ src/v2-routes/v2-upcoming.js
@@ -2,20 +2,20 @@
 
 const express = require("express")
 const v2 = express.Router()
+const asyncHandle = require("express-async-handler")
 const launch = require("../builders/launch-query")
 const launch_sort = require("../builders/launch-sort")
 
 // Upcoming launches by date, year, or all
-v2.get("/", (req, res, next) => {
+v2.get("/", asyncHandle(async (req, res) => {
   const query = launch.launchQuery(req)
   const sort = launch_sort.launchSort(req)
-  global.db.collection("upcoming_v2").find(query,{"_id": 0 }).sort(sort)
-    .toArray((err, doc) => {
-      if (err) {
-        return next(err)
-      }
-      res.json(doc)
-    })
-})
+  const data = await global.db
+    .collection("upcoming_v2")
+    .find(query,{"_id": 0 })
+    .sort(sort)
+    .toArray()
+  res.json(data)
+}))
 
 module.exports = v2
