cd16cf35c920aa84349e267a601729b87769c114
Jake Meyer <jakewmeyer@gmail.com>
2018-01-24 09:06:10 -0600

    update home to async

--- src/v2-routes/v2-home.js
+++ src/v2-routes/v2-home.js
@@ -1,16 +1,16 @@
-// Home Info Endpoint
+// API Info Endpoint
 
 const express = require("express")
 const v2 = express.Router()
+const asyncHandle = require("express-async-handler")
 
-// Returns API Info
-v2.get("/", (req, res, next) => {
-  global.db.collection("home").find({},{"_id": 0 }).toArray((err, doc) => {
-    if (err) {
-      return next(err)
-    }
-    res.json(doc[0])
-  })
-})
+// Returns API info
+v2.get("/", asyncHandle(async (req, res) => {
+  const data = await global.db
+    .collection("home")
+    .find({},{"_id": 0 })
+    .toArray()
+  res.json(data[0])
+}))
 
 module.exports = v2
