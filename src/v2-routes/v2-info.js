789bab22ee8b65ff9880d5ce0345b3624d1d8130
Jake Meyer <jakewmeyer@gmail.com>
2018-01-24 09:08:59 -0600

    update info to async

--- src/v2-routes/v2-info.js
+++ src/v2-routes/v2-info.js
@@ -2,15 +2,15 @@
 
 const express = require("express")
 const v2 = express.Router()
+const asyncHandle = require("express-async-handler")
 
 // Returns company info
-v2.get("/", (req, res, next) => {
-  global.db.collection("info").find({},{"_id": 0 }).toArray((err, doc) => {
-    if (err) {
-      return next(err)
-    }
-    res.json(doc[0])
-  })
-})
+v2.get("/", asyncHandle(async (req, res) => {
+  const data = await global.db
+    .collection("info")
+    .find({},{"_id": 0 })
+    .toArray()
+  res.json(data[0])
+}))
 
 module.exports = v2
