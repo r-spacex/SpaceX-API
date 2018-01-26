904cd688247f824361d3017ddcbbd5b5ab4acbca
Jake Meyer <jakewmeyer@gmail.com>
2018-01-26 09:26:31 -0600

    format launch sort with airbnb style

--- src/builders/launch-sort.js
+++ src/builders/launch-sort.js
@@ -2,18 +2,18 @@
  * Returns sorting query for past/upcoming launches
 */
 
-const lowerCase = require("lower-case")
+const lowerCase = require('lower-case');
 
 exports.launchSort = (req) => {
-  const query = {}
+  const query = {};
 
-  if (lowerCase(req.query.order) === "asc") {
-    query.flight_number = 1
-  } else if (lowerCase(req.query.order) === "desc") {
-    query.flight_number = -1
+  if (lowerCase(req.query.order) === 'asc') {
+    query.flight_number = 1;
+  } else if (lowerCase(req.query.order) === 'desc') {
+    query.flight_number = -1;
   } else {
-    query.flight_number = 1
+    query.flight_number = 1;
   }
 
-  return query
-}
+  return query;
+};
