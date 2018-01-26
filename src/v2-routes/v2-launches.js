cabc762e358cc210a6b3c2b65b14066112af7943
Jake Meyer <jakewmeyer@gmail.com>
2018-01-26 09:07:25 -0600

    format launches with airbnb style

--- src/v2-routes/v2-launches.js
+++ src/v2-routes/v2-launches.js
@@ -1,32 +1,33 @@
 // Launches Endpoints
 
-const express = require("express")
-const v2 = express.Router()
-const asyncHandle = require("express-async-handler")
-const launch = require("../builders/launch-query")
-const launch_sort = require("../builders/launch-sort")
+const express = require('express');
+const asyncHandle = require('express-async-handler');
+const launch = require('../builders/launch-query');
+const querySort = require('../builders/launch-sort');
+
+const v2 = express.Router();
 
 // Return most recent launch
-v2.get("/latest", asyncHandle(async (req, res) => {
+v2.get('/latest', asyncHandle(async (req, res) => {
   const data = await global.db
-    .collection("launch_v2")
-    .find({},{"_id": 0 })
-    .sort({"flight_number": -1})
+    .collection('launch_v2')
+    .find({}, { _id: 0 })
+    .sort({ flight_number: -1 })
     .limit(1)
-    .toArray()
-  res.json(data[0])
-}))
+    .toArray();
+  res.json(data[0]);
+}));
 
 // Return all past launches filtered by any querystring
-v2.get("/", asyncHandle(async (req, res) => {
-  const query = launch.launchQuery(req)
-  const sort = launch_sort.launchSort(req)
+v2.get('/', asyncHandle(async (req, res) => {
+  const query = launch.launchQuery(req);
+  const sort = querySort.launchSort(req);
   const data = await global.db
-    .collection("launch_v2")
-    .find(query,{"_id": 0 })
+    .collection('launch_v2')
+    .find(query, { _id: 0 })
     .sort(sort)
-    .toArray()
-  res.json(data)
-}))
+    .toArray();
+  res.json(data);
+}));
 
-module.exports = v2
+module.exports = v2;
