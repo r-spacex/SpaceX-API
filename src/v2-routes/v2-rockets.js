54ee04da9a66eb4e42d9e864a36a60fbb0407739
Jake Meyer <jakewmeyer@gmail.com>
2018-01-26 09:15:03 -0600

    format rockets with airbnb style

--- src/v2-routes/v2-rockets.js
+++ src/v2-routes/v2-rockets.js
@@ -1,26 +1,27 @@
 // Rocket Endpoints
 
-const express = require("express")
-const v2 = express.Router()
-const asyncHandle = require("express-async-handler")
+const express = require('express');
+const asyncHandle = require('express-async-handler');
+
+const v2 = express.Router();
 
 // Returns all rocket info
-v2.get("/", asyncHandle(async (req, res) => {
+v2.get('/', asyncHandle(async (req, res) => {
   const data = await global.db
-    .collection("rocket")
-    .find({},{"_id": 0 })
-    .toArray()
-  res.json(data)
-}))
+    .collection('rocket')
+    .find({}, { _id: 0 })
+    .toArray();
+  res.json(data);
+}));
 
 // Returns specific rocket info
-v2.get("/:rocket", asyncHandle(async (req, res) => {
-  const rocket = req.params.rocket
+v2.get('/:rocket', asyncHandle(async (req, res) => {
+  const param = req.params.rocket;
   const data = await global.db
-    .collection("rocket")
-    .find({"id": `${rocket}`},{"_id": 0 })
-    .toArray()
-  res.json(data[0])
-}))
+    .collection('rocket')
+    .find({ id: `${param}` }, { _id: 0 })
+    .toArray();
+  res.json(data[0]);
+}));
 
-module.exports = v2
+module.exports = v2;
