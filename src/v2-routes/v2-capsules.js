358859dffb176d4b90bca1b3acacdc6def63cee8
Jake Meyer <jakewmeyer@gmail.com>
2018-01-26 09:01:46 -0600

    format capsules with airbnb style

--- src/v2-routes/v2-capsules.js
+++ src/v2-routes/v2-capsules.js
@@ -1,26 +1,27 @@
 // Dragon Endpoints
 
-const express = require("express")
-const v2 = express.Router()
-const asyncHandle = require("express-async-handler")
+const express = require('express');
+const asyncHandle = require('express-async-handler');
+
+const v2 = express.Router();
 
 // Returns all Dragon data
-v2.get("/", asyncHandle(async (req, res) => {
+v2.get('/', asyncHandle(async (req, res) => {
   const data = await global.db
-    .collection("dragon")
-    .find({},{"_id": 0 })
-    .toArray()
-  res.json(data)
-}))
+    .collection('dragon')
+    .find({}, { _id: 0 })
+    .toArray();
+  res.json(data);
+}));
 
 // Returns specific Dragon data
-v2.get("/:capsule", asyncHandle(async (req, res) => {
-  const capsule = req.params.capsule
+v2.get('/:capsule', asyncHandle(async (req, res) => {
+  const cap = req.params.capsule;
   const data = await global.db
-    .collection("dragon")
-    .find({"id": capsule},{"_id": 0 })
-    .toArray()
-  res.json(data[0])
-}))
+    .collection('dragon')
+    .find({ id: cap }, { _id: 0 })
+    .toArray();
+  res.json(data[0]);
+}));
 
-module.exports = v2
+module.exports = v2;
