331666e35ceb9b3584de526241c060180402b5d2
Jake Meyer <jakewmeyer@gmail.com>
2018-01-26 09:17:29 -0600

    format upcoming with airbnb style

--- src/v2-routes/v2-upcoming.js
+++ src/v2-routes/v2-upcoming.js
@@ -1,21 +1,22 @@
 // Upcoming Endpoints
 
-const express = require("express")
-const v2 = express.Router()
-const asyncHandle = require("express-async-handler")
-const launch = require("../builders/launch-query")
-const launch_sort = require("../builders/launch-sort")
+const express = require('express');
+const asyncHandle = require('express-async-handler');
+const launch = require('../builders/launch-query');
+const querySort = require('../builders/launch-sort');
+
+const v2 = express.Router();
 
 // Upcoming launches by date, year, or all
-v2.get("/", asyncHandle(async (req, res) => {
-  const query = launch.launchQuery(req)
-  const sort = launch_sort.launchSort(req)
+v2.get('/', asyncHandle(async (req, res) => {
+  const query = launch.launchQuery(req);
+  const sort = querySort.launchSort(req);
   const data = await global.db
-    .collection("upcoming_v2")
-    .find(query,{"_id": 0 })
+    .collection('upcoming_v2')
+    .find(query, { _id: 0 })
     .sort(sort)
-    .toArray()
-  res.json(data)
-}))
+    .toArray();
+  res.json(data);
+}));
 
-module.exports = v2
+module.exports = v2;
