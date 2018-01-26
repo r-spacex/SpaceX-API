e22e80f6ef3ece419d243c165fed3964a09cac64
Jake Meyer <jakewmeyer@gmail.com>
2018-01-26 09:03:40 -0600

    format info with airbnb style

--- src/v2-routes/v2-info.js
+++ src/v2-routes/v2-info.js
@@ -1,16 +1,17 @@
 // Basic Info Endpoints
 
-const express = require("express")
-const v2 = express.Router()
-const asyncHandle = require("express-async-handler")
+const express = require('express');
+const asyncHandle = require('express-async-handler');
+
+const v2 = express.Router();
 
 // Returns company info
-v2.get("/", asyncHandle(async (req, res) => {
+v2.get('/', asyncHandle(async (req, res) => {
   const data = await global.db
-    .collection("info")
-    .find({},{"_id": 0 })
-    .toArray()
-  res.json(data[0])
-}))
+    .collection('info')
+    .find({}, { _id: 0 })
+    .toArray();
+  res.json(data[0]);
+}));
 
-module.exports = v2
+module.exports = v2;
