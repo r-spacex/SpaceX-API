0f4d0731f734435b4b259acae5a08e0bcda87307
Jake Meyer <jakewmeyer@gmail.com>
2018-01-26 09:09:43 -0600

    format launchpads with airbnb style

--- src/v2-routes/v2-launchpad.js
+++ src/v2-routes/v2-launchpad.js
@@ -1,26 +1,27 @@
 // Launchpad Endpoints
 
-const express = require("express")
-const v2 = express.Router()
-const asyncHandle = require("express-async-handler")
+const express = require('express');
+const asyncHandle = require('express-async-handler');
+
+const v2 = express.Router();
 
 // Return all launchpads
-v2.get("/", asyncHandle(async (req, res) => {
+v2.get('/', asyncHandle(async (req, res) => {
   const data = await global.db
-    .collection("launchpad")
-    .find({},{"_id": 0 })
-    .toArray()
-  res.json(data)
-}))
+    .collection('launchpad')
+    .find({}, { _id: 0 })
+    .toArray();
+  res.json(data);
+}));
 
 // Return specific launchpad
-v2.get("/:pad", asyncHandle(async (req, res) => {
-  const id = req.params.pad
+v2.get('/:pad', asyncHandle(async (req, res) => {
+  const id = req.params.pad;
   const data = await global.db
-    .collection("launchpad")
-    .find({"id": `${id}`}, {"_id": 0 })
-    .toArray()
-  res.json(data)
-}))
+    .collection('launchpad')
+    .find({ id: `${id}` }, { _id: 0 })
+    .toArray();
+  res.json(data);
+}));
 
-module.exports = v2
+module.exports = v2;
