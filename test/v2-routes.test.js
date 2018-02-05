5314f992cc0256b502803e24d5e6be5e7f713691
Jake Meyer <jakewmeyer@gmail.com>
2018-02-05 03:05:23 -0600

    move v2 launchpad test to unique file

--- test/v2-routes.test.js
+++ test/v2-routes.test.js
@@ -10,38 +10,6 @@ beforeAll((done) => {
 });
 
 //------------------------------------------------------------
-//                     Launchpads V2
-//------------------------------------------------------------
-
-test('It should return all launchpads', () => {
-  return request(app).get('/v2/launchpads').then((response) => {
-    expect(response.statusCode).toBe(200);
-    expect(response.body).toHaveLength(8);
-    response.body.forEach((item) => {
-      expect(item).toHaveProperty('id');
-      expect(item).toHaveProperty('full_name');
-      expect(item).toHaveProperty('status');
-      expect(item).toHaveProperty('vehicles_launched');
-      expect(item).toHaveProperty('details');
-    });
-  });
-});
-
-test('It should return LC-39A info', () => {
-  return request(app).get('/v2/launchpads/ksc_lc_39a').then((response) => {
-    expect(response.statusCode).toBe(200);
-    expect(response.text).toContain('ksc_lc_39a');
-  });
-});
-
-test('It should return no launchpad info', () => {
-  return request(app).get('/v2/launchpads/ksc_lc_40a').then((response) => {
-    expect(response.statusCode).toBe(200);
-    expect(response.text).toContain('');
-  });
-});
-
-//------------------------------------------------------------
 //                    Past Launches V2
 //------------------------------------------------------------
 
