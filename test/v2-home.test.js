090456ddc898b7edf5290d46a8e60f9937ba59b5
Jake Meyer <jakewmeyer@gmail.com>
2018-02-05 02:53:51 -0600

    move v2 home test to unique file

---
+++ test/v2-home.test.js
@@ -0,0 +1,25 @@
+
+const request = require('supertest');
+const app = require('../src/app');
+
+beforeAll((done) => {
+  app.on('ready', () => {
+    done();
+  });
+});
+
+//------------------------------------------------------------
+//                     Home Page V2
+//------------------------------------------------------------
+
+test('It should return home info', () => {
+  return request(app).get('/v2').then((response) => {
+    expect(response.statusCode).toBe(200);
+    expect(response.body).toHaveProperty('description');
+    expect(response.body).toHaveProperty('organization', 'r/SpaceX');
+    expect(response.body).toHaveProperty('organization_link', 'https://github.com/r-spacex');
+    expect(response.body).toHaveProperty('project_link', 'https://github.com/r-spacex/SpaceX-API');
+    expect(response.body).toHaveProperty('project_name', 'SpaceX-API');
+    expect(response.body).toHaveProperty('version');
+  });
+});
--- test/v2-routes.test.js
+++ test/v2-routes.test.js
@@ -10,22 +10,6 @@ beforeAll((done) => {
 });
 
 //------------------------------------------------------------
-//                     Home Page V2
-//------------------------------------------------------------
-
-test('It should return home info', () => {
-  return request(app).get('/v2').then((response) => {
-    expect(response.statusCode).toBe(200);
-    expect(response.body).toHaveProperty('description');
-    expect(response.body).toHaveProperty('organization', 'r/SpaceX');
-    expect(response.body).toHaveProperty('organization_link', 'https://github.com/r-spacex');
-    expect(response.body).toHaveProperty('project_link', 'https://github.com/r-spacex/SpaceX-API');
-    expect(response.body).toHaveProperty('project_name', 'SpaceX-API');
-    expect(response.body).toHaveProperty('version');
-  });
-});
-
-//------------------------------------------------------------
 //                     Company Info V2
 //------------------------------------------------------------
 
