3384c808feedc7536077ed2a81e36fbfe0cfc2e7
Jake Meyer <jakewmeyer@gmail.com>
2018-03-02 14:58:06 -0600

    Added jsdoc to helpers

--- src/helpers/launch-database.js
+++ src/helpers/launch-database.js
@@ -3,6 +3,13 @@ const launch = require('../builders/launch-query');
 const sort = require('../builders/launch-sort');
 const project = require('../builders/project-query');
 
+/**
+ * Fetches past or upcoming launches with built queries
+ * @param {string} collection The Mongo collection to search
+ * @param {Object} req The Express request object to build queries
+ * @return {Array} The Mongo array of objects
+ */
+
 module.exports.fetchLaunch = async (collection, req) => {
   return global.db
     .collection(collection)
