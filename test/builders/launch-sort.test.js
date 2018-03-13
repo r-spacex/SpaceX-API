6ba18809391453c57a5174708fa828c04e2b5366
Jake Meyer <jakewmeyer@gmail.com>
2018-03-12 21:00:41 -0500

    update launch-sort test with async

--- test/builders/launch-sort.test.js
+++ test/builders/launch-sort.test.js
@@ -12,16 +12,14 @@ beforeAll((done) => {
 //                     Launch Sort Test
 //------------------------------------------------------------
 
-test('It should return launches sorted from smallest to greatest', () => {
-  return request(app).get('/v2/launches?order=asc').then((response) => {
-    expect(response.statusCode).toBe(200);
-    expect(response.body[0]).toHaveProperty('flight_number', 1);
-  });
+test('It should return launches sorted from smallest to greatest', async () => {
+  const response = await request(app).get('/v2/launches?order=asc');
+  expect(response.statusCode).toBe(200);
+  expect(response.body[0]).toHaveProperty('flight_number', 1);
 });
 
-test('It should return launches sorted from greatest to smallest', () => {
-  return request(app).get('/v2/launches?order=desc').then((response) => {
-    expect(response.statusCode).toBe(200);
-    expect(response.body[response.body.length - 1]).toHaveProperty('flight_number', 1);
-  });
+test('It should return launches sorted from greatest to smallest', async () => {
+  const response = await request(app).get('/v2/launches?order=desc')
+  expect(response.statusCode).toBe(200);
+  expect(response.body[response.body.length - 1]).toHaveProperty('flight_number', 1);
 });
