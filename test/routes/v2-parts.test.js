34ce496ecba49add203ee28e000c9c5f62ef682b
Jake Meyer <jakewmeyer@gmail.com>
2018-03-12 20:11:58 -0500

    update parts test with async

--- test/routes/v2-parts.test.js
+++ test/routes/v2-parts.test.js
@@ -12,54 +12,50 @@ beforeAll((done) => {
 //                     Capsule V2
 //------------------------------------------------------------
 
-test('It should return all v2 capsules', () => {
-  return request(app).get('/v2/parts/caps').then((response) => {
-    expect(response.statusCode).toBe(200);
-    response.body.forEach((item) => {
-      expect(item).toHaveProperty('capsule_serial');
-      expect(item).toHaveProperty('capsule_id');
-      expect(item).toHaveProperty('status');
-      expect(item).toHaveProperty('original_launch');
-      expect(item.missions.length).toBeGreaterThan(0);
-      expect(item).toHaveProperty('landings');
-      expect(item).toHaveProperty('type');
-      expect(item).toHaveProperty('details');
-    });
+test('It should return all v2 capsules',  async () => {
+  const response = await request(app).get('/v2/parts/caps');
+  expect(response.statusCode).toBe(200);
+  response.body.forEach((item) => {
+    expect(item).toHaveProperty('capsule_serial');
+    expect(item).toHaveProperty('capsule_id');
+    expect(item).toHaveProperty('status');
+    expect(item).toHaveProperty('original_launch');
+    expect(item.missions.length).toBeGreaterThan(0);
+    expect(item).toHaveProperty('landings');
+    expect(item).toHaveProperty('type');
+    expect(item).toHaveProperty('details');
   });
 });
 
-test('It should return capsule C101', () => {
-  return request(app).get('/v2/parts/caps/C101').then((response) => {
-    expect(response.statusCode).toBe(200);
-    expect(response.body).toHaveProperty('capsule_serial', 'C101');
-  });
+test('It should return capsule C101', async () => {
+  const response = await request(app).get('/v2/parts/caps/C101');
+  expect(response.statusCode).toBe(200);
+  expect(response.body).toHaveProperty('capsule_serial', 'C101');
 });
 
 //------------------------------------------------------------
 //                     Core V2
 //------------------------------------------------------------
 
-test('It should return all v2 cores', () => {
-  return request(app).get('/v2/parts/cores').then((response) => {
-    expect(response.statusCode).toBe(200);
-    response.body.forEach((item) => {
-      expect(item).toHaveProperty('core_serial');
-      expect(item).toHaveProperty('status');
-      expect(item).toHaveProperty('original_launch');
-      expect(item.missions.length).toBeGreaterThan(0);
-      expect(item).toHaveProperty('rtls_attempt');
-      expect(item).toHaveProperty('rtls_landings');
-      expect(item).toHaveProperty('asds_attempt');
-      expect(item).toHaveProperty('asds_landings');
-      expect(item).toHaveProperty('water_landing');
-      expect(item).toHaveProperty('details');
-    });
+test('It should return all v2 cores', async () => {
+  const response = await request(app).get('/v2/parts/cores');
+  expect(response.statusCode).toBe(200);
+  response.body.forEach((item) => {
+    expect(item).toHaveProperty('core_serial');
+    expect(item).toHaveProperty('status');
+    expect(item).toHaveProperty('original_launch');
+    expect(item.missions.length).toBeGreaterThan(0);
+    expect(item).toHaveProperty('rtls_attempt');
+    expect(item).toHaveProperty('rtls_landings');
+    expect(item).toHaveProperty('asds_attempt');
+    expect(item).toHaveProperty('asds_landings');
+    expect(item).toHaveProperty('water_landing');
+    expect(item).toHaveProperty('details');
   });
 });
 
-test('It should return core B0007', () => {
-  return request(app).get('/v2/parts/cores/B0007').then((response) => {
-    expect(response.statusCode).toBe(200);
-    expect(response.body).toHaveProperty('core_serial', 'B0007');
-  });
+test('It should return core B0007', async () => {
+  const response = await request(app).get('/v2/parts/cores/B0007')
+  expect(response.statusCode).toBe(200);
+  expect(response.body).toHaveProperty('core_serial', 'B0007');
 });
