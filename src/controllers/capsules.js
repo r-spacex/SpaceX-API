81a1ac360690488ab850e70a9bb298da507991f3
Jake Meyer <jakewmeyer@gmail.com>
2018-04-09 06:38:07 -0500

    create capsules route controller

---
+++ src/controllers/capsules.js
@@ -0,0 +1,28 @@
+
+module.exports = {
+
+  /**
+   * Returns all Dragon data
+   */
+  all: async (ctx) => {
+    const data = await global.db
+      .collection('dragon')
+      .find({})
+      .project({ _id: 0 })
+      .toArray();
+    ctx.body = data;
+  },
+
+  /**
+   * Returns specific Dragon data
+   */
+  specific: async (ctx) => {
+    const data = await global.db
+      .collection('dragon')
+      .find({ id: ctx.params.capsule })
+      .project({ _id: 0 })
+      .toArray();
+    ctx.body = data[0];
+  },
+
+};
--- src/routes/v2-capsules.js
+++ src/routes/v2-capsules.js
@@ -1,29 +1,16 @@
 // Dragon Endpoints
 
 const Router = require('koa-router');
+const capsules = require('../controllers/capsules');
 
 const v2 = new Router({
   prefix: '/v2/capsules',
 });
 
 // Returns all Dragon data
-v2.get('/', async (ctx) => {
-  const data = await global.db
-    .collection('dragon')
-    .find({})
-    .project({ _id: 0 })
-    .toArray();
-  ctx.body = data;
-});
+v2.get('/', capsules.all);
 
 // Returns specific Dragon data
-v2.get('/:capsule', async (ctx) => {
-  const data = await global.db
-    .collection('dragon')
-    .find({ id: ctx.params.capsule })
-    .project({ _id: 0 })
-    .toArray();
-  ctx.body = data[0];
-});
+v2.get('/:capsule', capsules.specific);
 
 module.exports = v2;
