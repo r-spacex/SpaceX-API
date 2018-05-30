fb6812907c5e5d222c67684341116f479c28e141
Jake Meyer <jakewmeyer@gmail.com>
2018-05-30 12:01:42 -0500

    allow null values in core/capsule mission fields

--- test/routes/v2-parts.test.js
+++ test/routes/v2-parts.test.js
@@ -20,7 +20,6 @@ test('It should return all v2 capsules', async () => {
     expect(item).toHaveProperty('capsule_id');
     expect(item).toHaveProperty('status');
     expect(item).toHaveProperty('original_launch');
-    expect(item.missions.length).toBeGreaterThan(0);
     expect(item).toHaveProperty('landings');
     expect(item).toHaveProperty('type');
     expect(item).toHaveProperty('details');
@@ -44,7 +43,6 @@ test('It should return all v2 cores', async () => {
     expect(item).toHaveProperty('core_serial');
     expect(item).toHaveProperty('status');
     expect(item).toHaveProperty('original_launch');
-    expect(item.missions.length).toBeGreaterThan(0);
     expect(item).toHaveProperty('rtls_attempt');
     expect(item).toHaveProperty('rtls_landings');
     expect(item).toHaveProperty('asds_attempt');
