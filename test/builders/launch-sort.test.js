bd2ad5519942df81186c443d7e9eae958e3a909f
Jake Meyer <jakewmeyer@gmail.com>
2018-06-30 18:51:38 -0500

    Add tests to check for correct order in all launches

--- test/builders/launch-sort.test.js
+++ test/builders/launch-sort.test.js
@@ -12,14 +12,26 @@ beforeAll((done) => {
 //                     Launch Sort Test
 //------------------------------------------------------------
 
-test('It should return launches sorted from smallest to greatest', async () => {
+test('It should return past launches sorted from smallest to greatest', async () => {
   const response = await request(app.callback()).get('/v2/launches?order=asc');
   expect(response.statusCode).toBe(200);
   expect(response.body[0]).toHaveProperty('flight_number', 1);
 });
 
-test('It should return launches sorted from greatest to smallest', async () => {
+test('It should return past launches sorted from greatest to smallest', async () => {
   const response = await request(app.callback()).get('/v2/launches?order=desc');
+  expect(response.statusCode).toBe(200);
+  expect(response.body[response.body.length - 1]).toHaveProperty('flight_number', 1);
+});
+
+test('It should return all launches sorted from greatest to smallest', async () => {
+  const response = await request(app.callback()).get('/v2/launches/all?order=asc');
+  expect(response.statusCode).toBe(200);
+  expect(response.body[0]).toHaveProperty('flight_number', 1);
+});
+
+test('It should return all launches sorted from greatest to smallest', async () => {
+  const response = await request(app.callback()).get('/v2/launches/all?order=desc');
   expect(response.statusCode).toBe(200);
   expect(response.body[response.body.length - 1]).toHaveProperty('flight_number', 1);
 });
