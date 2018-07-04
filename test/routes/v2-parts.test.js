d583b98b16e0ac3cb4b878d6a082f38c4a3df27d
Jake Meyer <jakewmeyer@gmail.com>
2018-07-04 03:25:51 -0500

    add tests for new unix times in cores and caps

--- test/routes/v2-parts.test.js
+++ test/routes/v2-parts.test.js
@@ -20,6 +20,7 @@ test('It should return all v2 capsules', async () => {
     expect(item).toHaveProperty('capsule_id');
     expect(item).toHaveProperty('status');
     expect(item).toHaveProperty('original_launch');
+    expect(item).toHaveProperty('original_launch_unix');
     expect(item).toHaveProperty('landings');
     expect(item).toHaveProperty('type');
     expect(item).toHaveProperty('details');
@@ -43,6 +44,7 @@ test('It should return all v2 cores', async () => {
     expect(item).toHaveProperty('core_serial');
     expect(item).toHaveProperty('status');
     expect(item).toHaveProperty('original_launch');
+    expect(item).toHaveProperty('original_launch_unix');
     expect(item).toHaveProperty('rtls_attempt');
     expect(item).toHaveProperty('rtls_landings');
     expect(item).toHaveProperty('asds_attempt');
