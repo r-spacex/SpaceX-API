37a24bca77d418b4078cdd2e4474cee03fc520bc
Jake Meyer <jakewmeyer@gmail.com>
2018-07-10 12:02:53 -0500

    Allow mongo connection to use new url parser to remove deprecation warning

--- src/app.js
+++ src/app.js
@@ -71,7 +71,7 @@ module.exports = app;
 // Mongo Connection + Server Start
 (async () => {
   try {
-    const client = await MongoClient.connect(url, { poolSize: 20 });
+    const client = await MongoClient.connect(url, { poolSize: 20, useNewUrlParser: true });
 
     global.db = client.db('spacex-api');
 
