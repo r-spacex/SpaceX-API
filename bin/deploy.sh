fe324f57091fe3241b3aff661b7886404f9be90f
Jake Meyer <jakewmeyer@gmail.com>
2018-07-29 20:49:22 -0500

    fix permissions error

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -2,8 +2,6 @@
 
 # Import the SSH deployment key
 openssl aes-256-cbc -K $encrypted_32b6c04b6d48_key -iv $encrypted_32b6c04b6d48_iv -in deploy-key.enc -out ~/.ssh/deploy-key -d
-rm deploy-key.enc
-chmod 600 deploy-key
 mv deploy-key ~/.ssh/id_rsa
 
 ssh $SERVER_USER@$SERVER_IP "cd SpaceX-API && git checkout master && git pull && yarn --production && pm2 reload spacex-api"
