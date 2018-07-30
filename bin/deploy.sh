a1e1298ab46f77ff71fea1e741c22ac80b6f92fb
Jake Meyer <jakewmeyer@gmail.com>
2018-07-29 21:01:04 -0500

    more deployment script updates

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -2,6 +2,8 @@
 
 # Import the SSH deployment key
 openssl aes-256-cbc -K $encrypted_32b6c04b6d48_key -iv $encrypted_32b6c04b6d48_iv -in deploy-key.enc -out ~/.ssh/deploy-key -d
-mv deploy-key ~/.ssh/id_rsa
+eval "$(ssh-agent -s)" # Start ssh-agent cache
+chmod 600 .travis/deploy-key # Allow read access to the private key
+ssh-add .travis/deploy-key # Add the private key to SSH
 
-ssh $SERVER_USER@$SERVER_IP:22 "cd SpaceX-API && git checkout master && git pull && yarn --production && pm2 reload spacex-api"
+ssh $SERVER_USER@$SERVER_IP "cd SpaceX-API && git checkout master && git pull && yarn --production && pm2 reload spacex-api"
