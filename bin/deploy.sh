3656945a2a1a933a44ab7d6b6c3c5338bb4536c6
Jake Meyer <jakewmeyer@gmail.com>
2018-07-29 21:29:00 -0500

    fix deploy file paths

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
 # Import the SSH deployment key
-openssl aes-256-cbc -K $encrypted_32b6c04b6d48_key -iv $encrypted_32b6c04b6d48_iv -in deploy-key.enc -out ~/.ssh/deploy-key -d
+openssl aes-256-cbc -K $encrypted_32b6c04b6d48_key -iv $encrypted_32b6c04b6d48_iv -in deploy-key.enc -out ~/deploy-key -d
+chmod 600 ~/deploy-key
 eval "$(ssh-agent -s)" # Start ssh-agent cache
-ssh-add .travis/deploy-key # Add the private key to SSH
+ssh-add ~/deploy-key # Add the private key to SSH
 
 ssh -o StrictHostKeyChecking=no $SERVER_USER@$SERVER_IP "cd SpaceX-API && git checkout master && git pull && yarn --production && pm2 reload spacex-api"
