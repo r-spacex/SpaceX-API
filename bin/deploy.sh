5fe4f12a92efa06ae3d57a34372ef70c3de537ad
Jake Meyer <jakewmeyer@gmail.com>
2018-07-29 20:53:21 -0500

    Add port number to deploy

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -4,4 +4,4 @@
 openssl aes-256-cbc -K $encrypted_32b6c04b6d48_key -iv $encrypted_32b6c04b6d48_iv -in deploy-key.enc -out ~/.ssh/deploy-key -d
 mv deploy-key ~/.ssh/id_rsa
 
-ssh $SERVER_USER@$SERVER_IP "cd SpaceX-API && git checkout master && git pull && yarn --production && pm2 reload spacex-api"
+ssh $SERVER_USER@$SERVER_IP:22 "cd SpaceX-API && git checkout master && git pull && yarn --production && pm2 reload spacex-api"
