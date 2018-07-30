06d24148c2eff41088ab347bbfbb4b99c5e17a41
Jake Meyer <jakewmeyer@gmail.com>
2018-07-29 20:10:03 -0500

    Add deploy script

---
+++ bin/deploy.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# Import the SSH deployment key
+openssl aes-256-cbc -K $encrypted_32b6c04b6d48_key -iv $encrypted_32b6c04b6d48_iv -in deploy-key.enc -out ~\/.ssh/deploy-key -d
+rm deploy-key.enc
+chmod 600 deploy-key
+mv deploy-key ~/.ssh/id_rsa
+
+ssh $SERVER_USER@$SERVER_IP "rm -rf SpaceX-API && git clone https://github.com/r-spacex/SpaceX-API.git && cd SpaceX-API && yarn --production && pm2 reload spacex-api"
