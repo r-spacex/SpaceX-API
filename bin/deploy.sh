29ed3e37e6cd25934a241cf4a52e1da30d011053
Jake Meyer <jakewmeyer@gmail.com>
2018-07-29 21:19:50 -0500

    turn off key checking in deploy script

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -3,7 +3,6 @@
 # Import the SSH deployment key
 openssl aes-256-cbc -K $encrypted_32b6c04b6d48_key -iv $encrypted_32b6c04b6d48_iv -in deploy-key.enc -out ~/.ssh/deploy-key -d
 eval "$(ssh-agent -s)" # Start ssh-agent cache
-chmod 600 .travis/deploy-key # Allow read access to the private key
 ssh-add .travis/deploy-key # Add the private key to SSH
 
-ssh $SERVER_USER@$SERVER_IP "cd SpaceX-API && git checkout master && git pull && yarn --production && pm2 reload spacex-api"
+ssh -o StrictHostKeyChecking=no $SERVER_USER@$SERVER_IP "cd SpaceX-API && git checkout master && git pull && yarn --production && pm2 reload spacex-api"
