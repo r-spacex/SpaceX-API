122652fe348ce88c0e102b88ab906d6fdc840db4
Jake Meyer <jakewmeyer@gmail.com>
2018-08-02 16:16:29 -0500

    Add test for roadster data

--- test/routes/v2-info.test.js
+++ test/routes/v2-info.test.js
@@ -32,3 +32,27 @@ test('It should return company info', async () => {
   expect(response.body).toHaveProperty('headquarters.state', 'California');
   expect(response.body).toHaveProperty('summary');
 });
+
+//------------------------------------------------------------
+//                Falcon Heavy Roadster Info
+//------------------------------------------------------------
+
+test('It should return roadster info', async () => {
+  const response = await request(app.callback()).get('/v2/info/roadster');
+  expect(response.statusCode).toBe(200);
+  expect(response.body).toHaveProperty('name', 'Elon Musk\'s Tesla Roadster');
+  expect(response.body).toHaveProperty('apoapsis_au', expect.any(Number));
+  expect(response.body).toHaveProperty('periapsis_au', expect.any(Number));
+  expect(response.body).toHaveProperty('semi_major_axis_au', expect.any(Number));
+  expect(response.body).toHaveProperty('eccentricity', expect.any(Number));
+  expect(response.body).toHaveProperty('inclination', expect.any(Number));
+  expect(response.body).toHaveProperty('longitude', expect.any(Number));
+  expect(response.body).toHaveProperty('periapsis_arg', expect.any(Number));
+  expect(response.body).toHaveProperty('period_days', expect.any(Number));
+  expect(response.body).toHaveProperty('speed_kph', expect.any(Number));
+  expect(response.body).toHaveProperty('speed_mph', expect.any(Number));
+  expect(response.body).toHaveProperty('earth_distance_km', expect.any(Number));
+  expect(response.body).toHaveProperty('earth_distance_mi', expect.any(Number));
+  expect(response.body).toHaveProperty('mars_distance_km', expect.any(Number));
+  expect(response.body).toHaveProperty('mars_distance_mi', expect.any(Number));
+});
