b50e2c6a4bb4041bcfcf7d28019d87461803b738
Jake Meyer <jakewmeyer@gmail.com>
2018-08-03 15:42:43 -0500

    Add deploy script branch and pull request checks before executing

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/env bash
+set -xe
 
-# Import the SSH deployment key
-openssl aes-256-cbc -K $encrypted_32b6c04b6d48_key -iv $encrypted_32b6c04b6d48_iv -in deploy-key.enc -out ~/deploy-key -d
-chmod 600 ~/deploy-key
-eval "$(ssh-agent -s)"
-ssh-add ~/deploy-key 
+if [ $TRAVIS_BRANCH == 'master' && $TRAVIS_PULL_REQUEST == "false" ] ; then
+    # Import the SSH deployment key
+    openssl aes-256-cbc -K $encrypted_32b6c04b6d48_key -iv $encrypted_32b6c04b6d48_iv -in deploy-key.enc -out ~/deploy-key -d
+    chmod 600 ~/deploy-key
+    eval "$(ssh-agent -s)"
+    ssh-add ~/deploy-key 
 
-# SSH, pull, reinstall deps, and restart pm2 process
-ssh -t -o StrictHostKeyChecking=no $SERVER_USER@$SERVER_IP "bash --login -c 'cd SpaceX-API && git checkout master && git pull && yarn --production && pm2 reload spacex-api'"
+    # SSH, pull, reinstall deps, and restart pm2 process
+    ssh -t -o StrictHostKeyChecking=no $SERVER_USER@$SERVER_IP "bash --login -c 'cd SpaceX-API && git checkout master && git pull -force && yarn --production && pm2 reload spacex-api'"
+else
+    echo "Not deploying, since this branch isn't master."
+fi
