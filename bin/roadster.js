8c46940fdf4e08c4f77f14367f5893ef8a23dbd0
Jake Meyer <jakewmeyer@gmail.com>
2018-08-04 00:17:59 -0500

    remove puppeteer and old roadster update script

--- bin/roadster.js
+++ bin/roadster.js
@@ -1,61 +1,27 @@
 #!/usr/bin/env node
 
-/**
- * This updater scrapes calculated roadster orbital data, strips everthing
- * but numbers, and updates a mongo document with the results
- */
+// TODO: Build requests for Orbital Elements, and Observer speeds for Mars and Earth
+// TODO: Parse JPL plaintext responses for data
 
-const MongoClient = require('mongodb');
-const puppeteer = require('puppeteer');
+// const MongoClient = require('mongodb');
 
-let client;
+// const updateMongo = async (data) => {
+//   let client;
+//   try {
+//     client = await MongoClient.connect(process.env.MONGO_URL, { useNewUrlParser: true });
+//     const db = client.db('spacex-api');
+//     const col = db.collection('info');
 
-(async () => {
-  try {
-    client = await MongoClient.connect(process.env.MONGO_URL, { useNewUrlParser: true });
-    const db = client.db('spacex-api');
-    const col = db.collection('info');
+//     await col.updateOne({ name: 'Elon Musk\'s Tesla Roadster' }, { $set: data });
+//     console.log('Updated!');
+//   } catch (err) {
+//     console.log(err.stack);
+//     process.exit(1);
+//   }
 
-    const browser = await puppeteer.launch({ args: ['--no-sandbox', '--disable-setuid-sandbox'] });
-    const page = await browser.newPage();
-    await page.goto('https://where-is-tesla-roadster.space/live');
-    await page.waitFor(5000);
+//   if (client) {
+//     client.close();
+//   }
+// };
 
-    let parsed = {};
-    let document;
-
-    // Create object for mongo update using HTML id's for each table element
-    // Speed + Distance values don't have an inner span element, hence the textContent swap
-    const data = await page.evaluate(() => {
-      parsed = {
-        apoapsis_au: parseFloat((document.querySelector('#data_ra_au').innerText).replace(/[a-z]/gi, '')),
-        periapsis_au: parseFloat((document.querySelector('#data_rp_au').innerText).replace(/[a-z]/gi, '')),
-        semi_major_axis_au: parseFloat((document.querySelector('#data_a_au').innerText).replace(/[a-z]/gi, '')),
-        eccentricity: parseFloat((document.querySelector('#data_e').innerText).replace(/[a-z]/gi, '')),
-        inclination: parseFloat((document.querySelector('#data_i').innerText).replace(/[a-z]/gi, '')),
-        longitude: parseFloat((document.querySelector('#data_O').innerText).replace(/[a-z]/gi, '')),
-        periapsis_arg: parseFloat((document.querySelector('#data_w').innerText).replace(/[a-z]/gi, '')),
-        period_days: parseFloat((document.querySelector('#data_T_d').innerText).replace(/[a-z]/gi, '')),
-        speed_kph: parseFloat((document.querySelector('#data_os_kmh').textContent).replace(/[a-z]/gi, '')),
-        speed_mph: parseFloat((document.querySelector('#data_os_mph').textContent).replace(/[a-z]/gi, '')),
-        earth_distance_km: parseFloat((document.querySelector('#data_de_km').textContent).replace(/[a-z]/gi, '')),
-        earth_distance_mi: parseFloat((document.querySelector('#data_de_mile').textContent).replace(/[a-z]/gi, '')),
-        mars_distance_km: parseFloat((document.querySelector('#data_dm_km').textContent).replace(/[a-z]/gi, '')),
-        mars_distance_mi: parseFloat((document.querySelector('#data_dm_mile').textContent).replace(/[a-z]/gi, '')),
-      };
-      return parsed;
-    });
-    console.log(data);
-    await browser.close();
-
-    await col.updateOne({ name: 'Elon Musk\'s Tesla Roadster' }, { $set: data });
-    console.log('Updated!');
-  } catch (err) {
-    console.log(err.stack);
-    process.exit(1);
-  }
-
-  if (client) {
-    client.close();
-  }
-})();
+// updateMongo();
