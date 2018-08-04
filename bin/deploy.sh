9d78cb1093782af0de9ed9e186a283dbf6855c63
Jake Meyer <jakewmeyer@gmail.com>
2018-08-04 00:23:14 -0500

    remove travis pull request check, since the branch would not appear as master

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -xe
 
-if [ $TRAVIS_BRANCH == 'master' && $TRAVIS_PULL_REQUEST == "false" ] ; then
+if [ $TRAVIS_BRANCH == 'master' ] then
     # Import the SSH deployment key
     openssl aes-256-cbc -K $encrypted_32b6c04b6d48_key -iv $encrypted_32b6c04b6d48_iv -in deploy-key.enc -out ~/deploy-key -d
     chmod 600 ~/deploy-key
@@ -11,5 +11,5 @@ if [ $TRAVIS_BRANCH == 'master' && $TRAVIS_PULL_REQUEST == "false" ] ; then
     # SSH, pull, reinstall deps, and restart pm2 process
     ssh -t -o StrictHostKeyChecking=no $SERVER_USER@$SERVER_IP "bash --login -c 'cd SpaceX-API && git checkout master && git pull -force && yarn --production && pm2 reload spacex-api'"
 else
-    echo "Not deploying, since this branch isn't master."
+    echo "Not sending, since this branch isn't master."
 fi
