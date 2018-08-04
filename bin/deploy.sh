98defad1da3593932c04e682003dd1aa74ba61b7
Jake Meyer <jakewmeyer@gmail.com>
2018-08-04 00:44:09 -0500

    typo fix

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -9,7 +9,7 @@ if [ $TRAVIS_BRANCH == 'master' ]; then
     ssh-add ~/deploy-key 
 
     # SSH, pull, reinstall deps, and restart pm2 process
-    ssh -t -o StrictHostKeyChecking=no $SERVER_USER@$SERVER_IP "bash --login -c 'cd SpaceX-API && git checkout master && git pull -force && yarn --production && pm2 reload spacex-api'"
+    ssh -t -o StrictHostKeyChecking=no $SERVER_USER@$SERVER_IP "bash --login -c 'cd SpaceX-API && git checkout master && git pull --force && yarn --production && pm2 reload spacex-api'"
 else
     echo "Not sending, since this branch isn't master."
 fi
