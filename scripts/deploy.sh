59b8068ecaea22b390da2a97ac6ffde173c04b24
Jake Meyer <jakewmeyer@gmail.com>
2018-08-18 05:25:49 -0500

    bash fixes

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,7 +6,7 @@ if [ "${TRAVIS_PULL_REQUEST}" != "false" ]; then
   exit 0
 fi
 
-if [$TRAVIS_BRANCH == 'master']; then
+if [ "${TRAVIS_BRANCH}" == 'master' ]; then
     # Import the SSH deployment key
     openssl aes-256-cbc -K $encrypted_32b6c04b6d48_key -iv $encrypted_32b6c04b6d48_iv -in deploy-key.enc -out ~/deploy-key -d
     chmod 600 ~/deploy-key
@@ -16,5 +16,5 @@ if [$TRAVIS_BRANCH == 'master']; then
     # SSH, pull, reinstall deps, and restart pm2 process
     ssh -t -o StrictHostKeyChecking=no $SERVER_USER@$SERVER_IP "bash --login -c 'cd /var/www/spacexdata.com/SpaceX-API && git checkout master && git pull --force && yarn --production && pm2 reload spacex-api'"
 else
-    echo "Not sending, since this branch isn't master."
+    echo "Not publishing, since this branch isn't master."
 fi
