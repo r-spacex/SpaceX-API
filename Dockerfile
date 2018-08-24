8d595f4592fc24813fad0c1250168087c58fca2c
Jake Meyer <jakewmeyer@gmail.com>
2018-08-24 18:57:38 -0500

    Removed old rediscloud options from middleware and dockerfile

--- Dockerfile
+++ Dockerfile
@@ -7,7 +7,6 @@ RUN yarn install --production
 # And then copy over node_modules, etc from that stage to the smaller base image
 FROM mhart/alpine-node:base
 LABEL maintainer="jakewmeyer@gmail.com"
-ENV REDISCLOUD_URL=redis://default:default@redis_db:6379
 WORKDIR /app
 COPY --from=0 /app .
 COPY . .
--- src/middleware/redis.js
+++ src/middleware/redis.js
@@ -1,114 +1,53 @@
 
-const url = require('url');
-
 /**
- * Declare options for Redis with remote server
+ * List Redis cache times for individual routes
  */
 
-let options;
-
-if (process.env.REDISCLOUD_URL) {
-  const redisURL = url.parse(process.env.REDISCLOUD_URL);
-  options = {
-    routes: [{
-      path: '/v2/launches',
-      expire: 30,
-    }, {
-      path: '/v2/launches/(.*)',
-      expire: 30,
-    }, {
-      path: '/v2/payloads',
-      expire: 30,
-    }, {
-      path: '/v2/payloads/(.*)',
-      expire: 30,
-    }, {
-      path: '/v2/parts/(.*)',
-      expire: 6000,
-    }, {
-      path: '/v2',
-      expire: 86400,
-    }, {
-      path: '/v2/info',
-      expire: 86400,
-    }, {
-      path: '/v2/info/(.*)',
-      expire: 300,
-    }, {
-      path: '/v2/launchpads/(.*)',
-      expire: 86400,
-    }, {
-      path: '/v2/launchpads',
-      expire: 86400,
-    }, {
-      path: '/v2/rockets',
-      expire: 86400,
-    }, {
-      path: '/v2/rockets/(.*)',
-      expire: 86400,
-    }, {
-      path: '/v2/capsules/(.*)',
-      expire: 86400,
-    }, {
-      path: '/v2/capsules',
-      expire: 86400,
-    }],
-    passParam: 'pretty',
-    redis: {
-      host: redisURL.hostname,
-      port: redisURL.port,
-      options: {
-        password: redisURL.auth.split(':')[1],
-      },
-    },
-  };
-} else {
-  options = {
-    routes: [{
-      path: '/v2/launches',
-      expire: 30,
-    }, {
-      path: '/v2/launches/(.*)',
-      expire: 30,
-    }, {
-      path: '/v2/payloads',
-      expire: 30,
-    }, {
-      path: '/v2/payloads/(.*)',
-      expire: 30,
-    }, {
-      path: '/v2/parts/(.*)',
-      expire: 6000,
-    }, {
-      path: '/v2',
-      expire: 86400,
-    }, {
-      path: '/v2/info',
-      expire: 86400,
-    }, {
-      path: '/v2/info/(.*)',
-      expire: 300,
-    }, {
-      path: '/v2/launchpads/(.*)',
-      expire: 86400,
-    }, {
-      path: '/v2/launchpads',
-      expire: 86400,
-    }, {
-      path: '/v2/rockets/(.*)',
-      expire: 86400,
-    }, {
-      path: '/v2/rockets',
-      expire: 86400,
-    }, {
-      path: '/v2/capsules/(.*)',
-      expire: 86400,
-    }, {
-      path: '/v2/capsules',
-      expire: 86400,
-    }],
-    passParam: 'pretty',
-  };
-}
+const options = {
+  routes: [{
+    path: '/v2/launches',
+    expire: 30,
+  }, {
+    path: '/v2/launches/(.*)',
+    expire: 30,
+  }, {
+    path: '/v2/payloads',
+    expire: 30,
+  }, {
+    path: '/v2/payloads/(.*)',
+    expire: 30,
+  }, {
+    path: '/v2/parts/(.*)',
+    expire: 6000,
+  }, {
+    path: '/v2',
+    expire: 86400,
+  }, {
+    path: '/v2/info',
+    expire: 86400,
+  }, {
+    path: '/v2/info/(.*)',
+    expire: 300,
+  }, {
+    path: '/v2/launchpads/(.*)',
+    expire: 86400,
+  }, {
+    path: '/v2/launchpads',
+    expire: 86400,
+  }, {
+    path: '/v2/rockets/(.*)',
+    expire: 86400,
+  }, {
+    path: '/v2/rockets',
+    expire: 86400,
+  }, {
+    path: '/v2/capsules/(.*)',
+    expire: 86400,
+  }, {
+    path: '/v2/capsules',
+    expire: 86400,
+  }],
+  passParam: 'pretty',
+};
 
 module.exports = options;
