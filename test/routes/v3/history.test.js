6f8bdce17cf22f0742adc19abd80b53ce9fa28db
Jake Meyer <jakewmeyer@gmail.com>
2018-09-06 07:39:53 -0500

    Added v3 history test

---
+++ test/routes/v3/history.test.js
@@ -0,0 +1,29 @@
+
+const request = require('supertest');
+const app = require('../../../src/app');
+
+beforeAll(done => {
+  app.on('ready', () => {
+    done();
+  });
+});
+
+//------------------------------------------------------------
+//                       History
+//------------------------------------------------------------
+
+test('It should return all history', async () => {
+  const response = await request(app.callback()).get('/v3/history');
+  expect(response.statusCode).toBe(200);
+  response.body.forEach(event => {
+    expect(event).toHaveProperty('id', expect.any(Number));
+    expect(event).toHaveProperty('title', expect.any(String));
+    expect(event).toHaveProperty('event_date_utc', expect.any(String));
+    expect(event).toHaveProperty('event_date_unix', expect.any(Number));
+    expect(event).toHaveProperty('flight_number');
+    expect(event).toHaveProperty('details', expect.any(String));
+    expect(event).toHaveProperty('links.reddit');
+    expect(event).toHaveProperty('links.article');
+    expect(event).toHaveProperty('links.wikipedia');
+  });
+});
