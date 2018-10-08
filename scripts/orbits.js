115192a91f582b1d40c6e0a18c9787bf7815fafe
Jake Meyer <jakewmeyer@gmail.com>
2018-10-08 03:38:24 -0500

    reduce try/catch size in orbit and ship updaters

--- scripts/orbits.js
+++ scripts/orbits.js
@@ -25,75 +25,76 @@ async function asyncForEach(array, callback) {
   let client;
   try {
     client = await MongoClient.connect(process.env.MONGO_URL, { useNewUrlParser: true });
-    const db = client.db('spacex-api');
-    const col = db.collection('launch');
-    const data = await col.find({ upcoming: false }).sort({ flight_number: 1 });
-
-    const id = [];
-    await data.forEach(launch => {
-      launch.rocket.second_stage.payloads.forEach(payload => {
-        if (payload.norad_id !== undefined && payload.norad_id.length !== 0) {
-          id.push(payload.norad_id[0]);
-        }
-      });
-    });
-
-    await request.post('https://www.space-track.org/ajaxauth/login', {
-      form: {
-        identity: process.env.LOGIN,
-        password: process.env.PASSWORD,
-      },
-      json: true,
-    });
-
-    const start = async () => {
-      await asyncForEach(id, async num => {
-        await sleep(5000);
-        const orbitData = await request(`https://www.space-track.org/basicspacedata/query/class/tle/NORAD_CAT_ID/${num}/limit/1`);
-        const orbit = JSON.parse(orbitData);
-
-        if (orbit[0] !== undefined && orbit.length !== 0) {
-          const update = {
-            epoch: new Date(orbit[0].EPOCH).toISOString(),
-            mean_motion: parseFloat(orbit[0].MEAN_MOTION),
-            raan: parseFloat(orbit[0].RA_OF_ASC_NODE),
-            arg_of_pericenter: parseFloat(orbit[0].ARG_OF_PERICENTER),
-            mean_anomaly: parseFloat(orbit[0].MEAN_ANOMALY),
-            semi_major_axis_km: parseFloat(orbit[0].SEMIMAJOR_AXIS),
-            eccentricity: parseFloat(orbit[0].ECCENTRICITY),
-            periapsis_km: parseFloat(orbit[0].PERIGEE),
-            apoapsis_km: parseFloat(orbit[0].APOGEE),
-            inclination_deg: parseFloat(orbit[0].INCLINATION),
-            period_min: parseFloat(orbit[0].PERIOD),
-          };
-          console.log(`Updating...${orbit[0].OBJECT_NAME}`);
-          console.log(update);
-          await col.updateOne({ 'rocket.second_stage.payloads.norad_id': num }, {
-            $set: {
-              'rocket.second_stage.payloads.$.orbit_params.epoch': update.epoch,
-              'rocket.second_stage.payloads.$.orbit_params.mean_motion': update.mean_motion,
-              'rocket.second_stage.payloads.$.orbit_params.raan': update.raan,
-              'rocket.second_stage.payloads.$.orbit_params.arg_of_pericenter': update.arg_of_pericenter,
-              'rocket.second_stage.payloads.$.orbit_params.mean_anomaly': update.mean_anomaly,
-              'rocket.second_stage.payloads.$.orbit_params.semi_major_axis_km': update.semi_major_axis_km,
-              'rocket.second_stage.payloads.$.orbit_params.eccentricity': update.eccentricity,
-              'rocket.second_stage.payloads.$.orbit_params.periapsis_km': update.periapsis_km,
-              'rocket.second_stage.payloads.$.orbit_params.apoapsis_km': update.apoapsis_km,
-              'rocket.second_stage.payloads.$.orbit_params.inclination_deg': update.inclination_deg,
-              'rocket.second_stage.payloads.$.orbit_params.period_min': update.period_min,
-            },
-          });
-          console.log('Updated...');
-        }
-      });
-    };
-    await start();
-
-    console.log(`${id.length} launch orbits updated!`);
   } catch (err) {
     console.log(err.stack);
     process.exit(1);
   }
+
+  const db = client.db('spacex-api');
+  const col = db.collection('launch');
+  const data = await col.find({ upcoming: false }).sort({ flight_number: 1 });
+
+  const id = [];
+  await data.forEach(launch => {
+    launch.rocket.second_stage.payloads.forEach(payload => {
+      if (payload.norad_id !== undefined && payload.norad_id.length !== 0) {
+        id.push(payload.norad_id[0]);
+      }
+    });
+  });
+
+  await request.post('https://www.space-track.org/ajaxauth/login', {
+    form: {
+      identity: process.env.LOGIN,
+      password: process.env.PASSWORD,
+    },
+    json: true,
+  });
+
+  const start = async () => {
+    await asyncForEach(id, async num => {
+      await sleep(5000);
+      const orbitData = await request(`https://www.space-track.org/basicspacedata/query/class/tle/NORAD_CAT_ID/${num}/limit/1`);
+      const orbit = JSON.parse(orbitData);
+
+      if (orbit[0] !== undefined && orbit.length !== 0) {
+        const update = {
+          epoch: new Date(orbit[0].EPOCH).toISOString(),
+          mean_motion: parseFloat(orbit[0].MEAN_MOTION),
+          raan: parseFloat(orbit[0].RA_OF_ASC_NODE),
+          arg_of_pericenter: parseFloat(orbit[0].ARG_OF_PERICENTER),
+          mean_anomaly: parseFloat(orbit[0].MEAN_ANOMALY),
+          semi_major_axis_km: parseFloat(orbit[0].SEMIMAJOR_AXIS),
+          eccentricity: parseFloat(orbit[0].ECCENTRICITY),
+          periapsis_km: parseFloat(orbit[0].PERIGEE),
+          apoapsis_km: parseFloat(orbit[0].APOGEE),
+          inclination_deg: parseFloat(orbit[0].INCLINATION),
+          period_min: parseFloat(orbit[0].PERIOD),
+        };
+        console.log(`Updating...${orbit[0].OBJECT_NAME}`);
+        console.log(update);
+        await col.updateOne({ 'rocket.second_stage.payloads.norad_id': num }, {
+          $set: {
+            'rocket.second_stage.payloads.$.orbit_params.epoch': update.epoch,
+            'rocket.second_stage.payloads.$.orbit_params.mean_motion': update.mean_motion,
+            'rocket.second_stage.payloads.$.orbit_params.raan': update.raan,
+            'rocket.second_stage.payloads.$.orbit_params.arg_of_pericenter': update.arg_of_pericenter,
+            'rocket.second_stage.payloads.$.orbit_params.mean_anomaly': update.mean_anomaly,
+            'rocket.second_stage.payloads.$.orbit_params.semi_major_axis_km': update.semi_major_axis_km,
+            'rocket.second_stage.payloads.$.orbit_params.eccentricity': update.eccentricity,
+            'rocket.second_stage.payloads.$.orbit_params.periapsis_km': update.periapsis_km,
+            'rocket.second_stage.payloads.$.orbit_params.apoapsis_km': update.apoapsis_km,
+            'rocket.second_stage.payloads.$.orbit_params.inclination_deg': update.inclination_deg,
+            'rocket.second_stage.payloads.$.orbit_params.period_min': update.period_min,
+          },
+        });
+        console.log('Updated...');
+      }
+    });
+  };
+  await start();
+
+  console.log(`${id.length} launch orbits updated!`);
 
   if (client) {
     client.close();
--- scripts/ships.js
+++ scripts/ships.js
@@ -27,75 +27,76 @@ async function asyncForEach(array, callback) {
   let client;
   try {
     client = await MongoClient.connect(process.env.MONGO_URL, { useNewUrlParser: true });
-    const db = client.db('spacex-api');
-    const col = db.collection('ship');
-    const data = await col.find({}).sort({ year_built: 1 });
-
-    // Gather individual ship mmsi numbers into array for requests
-    const id = [];
-    await data.forEach(ship => {
-      if (ship.mmsi != null) {
-        id.push(ship.mmsi);
-      }
-    });
-
-    const start = async () => {
-      await asyncForEach(id, async num => {
-        const result = await request(`https://www.marinetraffic.com/en/ais/details/ships/mmsi:${num}`);
-
-        const $ = cheerio.load(result);
-
-        // Get current lat/long and strip extra characters
-        // Raw output: 28.40871° / -80.59808°
-        const coordinates = $('#tabs-last-pos > div > div > div.table-cell.cell-full.collapse-768 > div:nth-child(4) > span:nth-child(2) > strong > a').text();
-        const parsed_coordinates = coordinates.replace('°', '').replace('°', '').split(' / ');
-
-        // Get current ship status fx. Stopped, Moored, Underway...
-        // Raw output: Underway Using Engine
-        const ship_status = $('#tabs-last-pos > div > div > div.table-cell.cell-full.collapse-768 > div:nth-child(5) > span:nth-child(2) > strong').text();
-
-        // Get current ship speed and direction and strip extra characters
-        // Raw output: 17.2kn / 67°
-        // If the speed is zero, the course returns '-'. This is replaced with null for zero speeds
-        const direction = $('#tabs-last-pos > div > div > div.table-cell.cell-full.collapse-768 > div:nth-child(6) > span:nth-child(2) > strong').text();
-        const parsed_direction = direction.replace('kn', '').replace('°', '').split(' / ');
-        let ship_course;
-        if (parsed_direction[1] === '-') {
-          ship_course = null;
-        } else {
-          ship_course = parseFloat(parsed_direction[1]);
-        }
-
-        const update = {
-          latitude: parseFloat(parsed_coordinates[0]),
-          longitude: parseFloat(parsed_coordinates[1]),
-          status: ship_status,
-          speed: parseFloat(parsed_direction[0]),
-          course: ship_course,
-        };
-
-        console.log(`Updating ${num}...`);
-        await col.updateOne({ mmsi: num }, {
-          $set: {
-            'position.latitude': update.latitude,
-            'position.longitude': update.longitude,
-            status: update.status,
-            speed_kn: update.speed,
-            course_deg: update.course,
-          },
-        });
-        console.log('Updated');
-
-        // Wait 5 seconds between page fetches to be respectful of bandwidth
-        await sleep(5000);
-      });
-    };
-    await start();
-    console.log(`Updated ${id.length} ships`);
   } catch (err) {
     console.log(err.stack);
     process.exit(1);
   }
+
+  const db = client.db('spacex-api');
+  const col = db.collection('ship');
+  const data = await col.find({}).sort({ year_built: 1 });
+
+  // Gather individual ship mmsi numbers into array for requests
+  const id = [];
+  await data.forEach(ship => {
+    if (ship.mmsi != null) {
+      id.push(ship.mmsi);
+    }
+  });
+
+  const start = async () => {
+    await asyncForEach(id, async num => {
+      const result = await request(`https://www.marinetraffic.com/en/ais/details/ships/mmsi:${num}`);
+
+      const $ = cheerio.load(result);
+
+      // Get current lat/long and strip extra characters
+      // Raw output: 28.40871° / -80.59808°
+      const coordinates = $('#tabs-last-pos > div > div > div.table-cell.cell-full.collapse-768 > div:nth-child(4) > span:nth-child(2) > strong > a').text();
+      const parsed_coordinates = coordinates.replace('°', '').replace('°', '').split(' / ');
+
+      // Get current ship status fx. Stopped, Moored, Underway...
+      // Raw output: Underway Using Engine
+      const ship_status = $('#tabs-last-pos > div > div > div.table-cell.cell-full.collapse-768 > div:nth-child(5) > span:nth-child(2) > strong').text();
+
+      // Get current ship speed and direction and strip extra characters
+      // Raw output: 17.2kn / 67°
+      // If the speed is zero, the course returns '-'. This is replaced with null for zero speeds
+      const direction = $('#tabs-last-pos > div > div > div.table-cell.cell-full.collapse-768 > div:nth-child(6) > span:nth-child(2) > strong').text();
+      const parsed_direction = direction.replace('kn', '').replace('°', '').split(' / ');
+      let ship_course;
+      if (parsed_direction[1] === '-') {
+        ship_course = null;
+      } else {
+        ship_course = parseFloat(parsed_direction[1]);
+      }
+
+      const update = {
+        latitude: parseFloat(parsed_coordinates[0]),
+        longitude: parseFloat(parsed_coordinates[1]),
+        status: ship_status,
+        speed: parseFloat(parsed_direction[0]),
+        course: ship_course,
+      };
+
+      console.log(`Updating ${num}...`);
+      await col.updateOne({ mmsi: num }, {
+        $set: {
+          'position.latitude': update.latitude,
+          'position.longitude': update.longitude,
+          status: update.status,
+          speed_kn: update.speed,
+          course_deg: update.course,
+        },
+      });
+      console.log('Updated');
+
+      // Wait 5 seconds between page fetches to be respectful of bandwidth
+      await sleep(5000);
+    });
+  };
+  await start();
+  console.log(`Updated ${id.length} ships`);
 
   if (client) {
     client.close();
