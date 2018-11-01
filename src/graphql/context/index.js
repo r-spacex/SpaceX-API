1951f6de7d484f12fecff291f9732f7d5f02f7cd
Carlos Rufo <sw@carlosrj.com>
2018-11-01 16:05:27 -0600

    add order builder to context

--- src/graphql/context/index.js
+++ src/graphql/context/index.js
@@ -1,9 +1,10 @@
 /*eslint-disable */
 const find = require('../../builders/v3/find')
 const limit = require('../../builders/v3/limit')
+const order = require('../../builders/v3/order')
 const sort = require('../../builders/v3/sort')
 const project = require('../../builders/v3/project')
 
-const context = { find, limit, sort, project }
+const context = { find, limit, order, sort, project }
 
 module.exports = context
