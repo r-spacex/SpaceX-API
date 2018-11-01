de2a8abc614b1ae7c3da6f15eaeec0c9f6912bb9
Carlos Rufo <sw@carlosrj.com>
2018-11-01 15:44:04 -0600

    add builders into context

--- src/graphql/context/index.js
+++ src/graphql/context/index.js
@@ -1,4 +1,9 @@
 /*eslint-disable */
-const context = {}
+const find = require('../../builders/v3/find')
+const limit = require('../../builders/v3/limit')
+const sort = require('../../builders/v3/sort')
+const project = require('../../builders/v3/project')
+
+const context = { find, limit, sort, project }
 
 module.exports = context
