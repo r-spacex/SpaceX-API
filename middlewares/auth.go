7bed170d62349c4fea1ede41074dc5547fa0885f
Jake Meyer <jakewmeyer@gmail.com>
2020-03-28 15:52:00 -0500

    add auth middleware for protected routes

--- middlewares/auth.go
+++ middlewares/auth.go
@@ -1,20 +1,45 @@
 package middlewares
 
 import (
+	"context"
 	"net/http"
 
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 )
+
+type User struct {
+	ID  primitive.ObjectID `bson:"_id"`
+	Key string             `bson:"key"`
+}
 
 func Auth(client *mongo.Client) func(next http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			// Check if token exists in database
+			// Return 400 on missing key
+			key := r.Header.Get("spacex-key")
+			if key == "" {
+				http.Error(w, "Invalid Request", http.StatusBadRequest)
+				return
+			}
 
-			// If it does, check that role is admin
+			// Auth requires explicit match
+			db := client.Database("auth")
+			user := &User{}
+			err := db.Collection("users").FindOne(context.TODO(), bson.M{"key": key}).Decode(&user)
+			if err != nil {
+				http.Error(w, "Unauthorized", http.StatusUnauthorized)
+				return
+			}
+			if user.Key == key {
+				next.ServeHTTP(w, r)
+				return
+			}
 
-			// If admin role, allow access
-			next.ServeHTTP(w, r)
+			// Default branch is 401
+			http.Error(w, "Unauthorized", http.StatusUnauthorized)
+			return
 		})
 	}
 }
