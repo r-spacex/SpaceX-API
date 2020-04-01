b83c31b1f49a15f7ea421bf61569ba7f80bb511b
Jake Meyer <jakewmeyer@gmail.com>
2020-04-01 18:36:13 -0500

    add payload endpoints

--- services/v4/payloads/routes.js
+++ services/v4/payloads/routes.js
@@ -4,7 +4,74 @@ const Payload = require('./model');
 const { auth } = require('../../../middleware');
 
 const router = new Router({
-  prefix: '/',
+  prefix: '/payloads',
+});
+
+// Get all payloads
+router.get('/', async (ctx) => {
+  try {
+    const result = await Payload.find({});
+    ctx.status = 200;
+    ctx.body = result;
+  } catch (error) {
+    ctx.throw(400, error.message);
+  }
+});
+
+// Get one payload
+router.get('/:id', async (ctx) => {
+  try {
+    const result = await Payload.findById(ctx.params.id);
+    ctx.status = 200;
+    ctx.body = result;
+  } catch (error) {
+    ctx.throw(400, error.message);
+  }
+});
+
+// Query payloads
+router.post('/query', async (ctx) => {
+  const { query, options } = ctx.request.body;
+  try {
+    const result = await Payload.paginate(query, options);
+    ctx.status = 200;
+    ctx.body = result;
+  } catch (error) {
+    ctx.throw(400, error.message);
+  }
+});
+
+// Create a payload
+router.post('/', auth, async (ctx) => {
+  try {
+    const core = new Payload(ctx.request.body);
+    await core.save();
+    ctx.status = 201;
+  } catch (error) {
+    ctx.throw(400, error.message);
+  }
+});
+
+// Update a payload
+router.patch('/:id', auth, async (ctx) => {
+  try {
+    await Payload.findByIdAndUpdate(ctx.params.id, ctx.request.body, {
+      runValidators: true,
+    });
+    ctx.status = 200;
+  } catch (error) {
+    ctx.throw(400, error.message);
+  }
+});
+
+// Delete a payload
+router.delete('/:id', auth, async (ctx) => {
+  try {
+    await Payload.findByIdAndDelete(ctx.params.id);
+    ctx.status = 200;
+  } catch (error) {
+    ctx.throw(400, error.message);
+  }
 });
 
 module.exports = router;
