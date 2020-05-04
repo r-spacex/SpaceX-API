42ddc850e082db9ed4b8ddbd2f77dd929f76e6db
Jake Meyer <jakewmeyer@gmail.com>
2020-05-03 22:43:54 -0500

    add job for payload orbits

--- jobs/worker.js
+++ jobs/worker.js
@@ -2,9 +2,12 @@
 const { CronJob } = require('cron');
 const webcast = require('./webcast');
 const launches = require('./launches');
+const payloads = require('./payloads');
 
-const webcastJob = new CronJob('*/10 * * * *', webcast);
-const launchesJob = new CronJob('*/10 * * * *', launches);
+const webcastJob = new CronJob('*/10 * * * *', webcast); // Every 10 minutes
+const launchesJob = new CronJob('*/10 * * * *', launches); // Every 10 minutes
+const payloadsJob = new CronJob('20 * * * *', payloads); // Every hour on :20
 
 webcastJob.start();
 launchesJob.start();
+payloadsJob.start();
