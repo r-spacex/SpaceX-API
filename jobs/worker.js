ac95a117e3f556587c53055e68047e977d667b4d
Jake Meyer <jakewmeyer@gmail.com>
2020-05-04 20:20:35 -0500

    add launchpads job

--- jobs/worker.js
+++ jobs/worker.js
@@ -4,13 +4,16 @@ const webcast = require('./webcast');
 const launches = require('./launches');
 const payloads = require('./payloads');
 const landpads = require('./landpads');
+const launchpads = require('./launchpads');
 
 const webcastJob = new CronJob('*/10 * * * *', webcast); // Every 10 minutes
 const launchesJob = new CronJob('*/10 * * * *', launches); // Every 10 minutes
 const payloadsJob = new CronJob('20 * * * *', payloads); // Every hour on :20
 const landpadsJob = new CronJob('*/10 * * * *', landpads); // Every 10 minutes
+const launchpadsJob = new CronJob('*/10 * * * *', launchpads); // Every 10 minutes
 
 webcastJob.start();
 launchesJob.start();
 payloadsJob.start();
 landpadsJob.start();
+launchpadsJob.start();
