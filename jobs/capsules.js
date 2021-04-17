e0569b36f23cf79354d13a126e06318ebfd080a2
Jake Meyer <jakewmeyer@gmail.com>
2021-04-16 19:10:26 -0500

    Update capsules job to pull last_update status from subreddit wiki

--- jobs/capsules.js
+++ jobs/capsules.js
@@ -1,9 +1,11 @@
 const got = require('got');
+const cheerio = require('cheerio');
 const { logger } = require('../middleware/logger');
 
 const API = process.env.SPACEX_API;
 const KEY = process.env.SPACEX_KEY;
 const HEALTHCHECK = process.env.CAPSULES_HEALTHCHECK;
+const REDDIT_CAPSULES = 'https://old.reddit.com/r/spacex/wiki/capsules';
 
 /**
  * Update capsule landings/reuse count
@@ -20,6 +22,19 @@ module.exports = async () => {
       resolveBodyOnly: true,
       responseType: 'json',
     });
+
+    const result = await got(REDDIT_CAPSULES);
+    const $ = cheerio.load(result.body);
+
+    const v1Capsules = $('body > div.content > div > div > table:nth-child(8) > tbody').text();
+    const v2Capsules = $('body > div.content > div > div > table:nth-child(10) > tbody').text();
+    const caps = v1Capsules.concat(v2Capsules);
+    const capsuleRow = caps.split('\n').filter((v) => v !== '');
+    const capsuleIds = capsuleRow.filter((value, index) => index % 7 === 0);
+    if (!capsuleIds.length) {
+      throw new Error('No capsules found');
+    }
+    const capsuleStatus = capsuleRow.filter((value, index) => (index + 1) % 7 === 0).map((x) => x.replace(/\[source\]/gi, ''));
 
     const updates = capsules.docs.map(async (capsule) => {
       const waterLandings = await got.post(`${API}/payloads/query`, {
@@ -50,11 +65,13 @@ module.exports = async () => {
         responseType: 'json',
       });
 
+      const index = capsuleIds.findIndex((id) => id === capsule.serial);
       await got.patch(`${API}/capsules/${capsule.id}`, {
         json: {
           reuse_count: capsule.launches.length,
           water_landings: waterLandings.totalDocs,
           land_landings: landLandings.totalDocs,
+          last_update: capsuleStatus[parseInt(index, 10)],
         },
         headers: {
           'spacex-key': KEY,
