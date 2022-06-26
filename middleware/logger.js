8d64e7362848817dc056966f631e1675fee14d87
Jake Meyer <jakewmeyer@gmail.com>
2022-06-26 13:13:48 -0500

    remove pino-pretty

--- middleware/logger.js
+++ middleware/logger.js
@@ -1,12 +1,6 @@
 import pino from 'pino';
 
-const opts = {
-  transport: {
-    target: 'pino-pretty',
-  },
-};
-
 const env = process.env.NODE_ENV;
 const isLocal = (env !== 'production' || env !== 'stage');
 
-export default isLocal ? pino(opts) : pino();
+export default isLocal ? pino() : pino();
